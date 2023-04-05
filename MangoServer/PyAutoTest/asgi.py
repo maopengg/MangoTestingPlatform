338d4691bca4e12b218025c93a945a1f1dc40b4e
毛鹏 <729164035@qq.com>
2023-04-05 09:33:22 +0800

    处理日志目录，如果没有新建logs目录则会自动新建

--- MangoServer/PyAutoTest/asgi.py
+++ MangoServer/PyAutoTest/asgi.py
@@ -15,8 +15,10 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 from PyAutoTest.auto_test.auto_system.scheduled_tasks.tasks import create_jobs
+from script.nuw_logs import __nuw_dir
 from . import routing
 
+# django自带，处理路由
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PyAutoTest.settings')
 django.setup()
 
@@ -24,4 +26,7 @@ application = ProtocolTypeRouter({
     'http': get_asgi_application(),
     'websocket': AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
 })
+# 定时任务
 create_jobs()
+# 处理日志文件
+__nuw_dir()
