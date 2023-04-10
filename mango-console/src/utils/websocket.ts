b5c033149c760c3ca811e9bef194f69e064cb0a1
毛鹏 <729164035@qq.com>
2023-04-10 21:54:11 +0800

    使用统一的格式化工具，格式化代码

--- mango-console/src/utils/websocket.ts
+++ mango-console/src/utils/websocket.ts
@@ -2,10 +2,15 @@
 import { reactive } from 'vue'
 import { Notification } from '@arco-design/web-vue'
 
+export const DRIVER = 'Mango Actuator'
+export const SERVER = 'Mango Server'
+export const WEB = 'mango-console'
+
 interface WebSocketServiceState {
   url: string
   socket: WebSocket | null
 }
+
 export default class WebSocketService {
   private state: WebSocketServiceState
 
@@ -20,32 +25,33 @@ export default class WebSocketService {
     this.state.socket = new WebSocket(this.state.url)
 
     this.state.socket.onopen = () => {
-      Notification.success('WebSocket服务已启动，开始接收服务器消息和发送指令给执行端！')
+      Notification.success(`Socket服务已启动，开始接收${SERVER}消息和发送指令给${DRIVER}！`)
       // 发送消息
       const message = {
         code: 200,
         func: null,
-        user_info: null,
-        msg: 'Hello WebSocket!',
-        data: null
+        user: null,
+        msg: `Hi, ${SERVER}, mango-console Request Connection!`,
+        data: null,
+        end: null
       }
       this.state.socket?.send(JSON.stringify(message))
     }
 
     this.state.socket.onmessage = (event) => {
       const res = JSON.parse(event.data)
-      console.log(res.msg)
-      Notification.info('WebSocket接收到消息：' + res.msg)
+      Notification.info('Socket消息：' + res.msg)
     }
 
     this.state.socket.onclose = () => {
-      Notification.error('WebSocket连接已关闭！')
+      Notification.error('Socket连接已关闭！')
     }
 
     this.state.socket.onerror = (error) => {
-      console.log('WebSocket发生错误：', error)
+      console.log('Socket发生错误：', error)
     }
   }
+
   // 关闭函数
   disconnect() {
     if (this.state.socket) {
@@ -53,14 +59,16 @@ export default class WebSocketService {
       this.state.socket = null
     }
   }
+
   // 发送消息函数
-  sendMessage(code: number, func: string, user_info: number, msg: string, data: object) {
+  sendMessage(code: number, func: string, user: number, msg: string, data: object, end: boolean) {
     const message = {
       code: code,
       func: func,
-      user_info: user_info,
+      user: user,
       msg: msg,
-      data: data
+      data: data,
+      end: end
     }
 
     if (this.state.socket) {
