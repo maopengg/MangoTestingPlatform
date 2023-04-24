e5e6f26af5c8d9e4ec4d43d4de9de78b32d3735b
maopeng <729164035@qq.com>
2023-04-24 18:22:00 +0800

    增加一个给windows弹窗的功能

--- mango-console/src/views/help/test.vue
+++ mango-console/src/views/help/test.vue
@@ -8,8 +8,24 @@
 </template>
 
 <script lang="ts" setup>
+import { Message } from '@arco-design/web-vue'
 import { ref } from 'vue'
 
 const value = ref('测试页面')
 const content = ref('')
+
+function sendMessage(msg = '执行失败') {
+  const title = '用例执行结果'
+  const msg1 = msg
+  console.log(msg1)
+  if (Notification.permission != 'granted') {
+    Notification.requestPermission()
+  }
+  if (Notification.permission === 'granted') {
+    const notification = new Notification(title, { body: msg1 })
+    notification.addEventListener('click', () => {
+      Message.success('请点击确认按钮，给浏览器授权发送消息到windows桌面！')
+    })
+  }
+}
 </script>
