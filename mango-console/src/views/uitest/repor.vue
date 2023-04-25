a8de9aff9e72c6cb957785daed2c26e9ef6ed962
maopeng <729164035@qq.com>
2023-04-25 21:21:06 +0800

    格式化代码

--- mango-console/src/views/uitest/repor.vue
+++ mango-console/src/views/uitest/repor.vue
@@ -1,7 +1,23 @@
 <template>
-  <a-card title="测试报告">
-    <a-input v-model="content" placeholder="请输入信息" />
-  </a-card>
+  <div>
+    <a-split
+      :style="{
+        height: '200px',
+        width: '100%',
+        minWidth: '500px',
+        border: '1px solid var(--color-border)'
+      }"
+      v-model:size="size"
+      min="80px"
+    >
+      <template #first>
+        <a-typography-paragraph>Left</a-typography-paragraph>
+      </template>
+      <template #second>
+        <a-typography-paragraph>Right</a-typography-paragraph>
+      </template>
+    </a-split>
+  </div>
 </template>
 
 <script lang="ts" setup>
