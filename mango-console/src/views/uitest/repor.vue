827a825e81a20d5dbb2ddd555661b68993f7cbee
maopeng <729164035@qq.com>
2023-04-25 22:28:31 +0800

    编写测试结果ui页面

--- mango-console/src/views/uitest/repor.vue
+++ mango-console/src/views/uitest/repor.vue
@@ -1,27 +1,77 @@
 <template>
   <div>
-    <a-split
-      :style="{
-        height: '200px',
-        width: '100%',
-        minWidth: '500px',
-        border: '1px solid var(--color-border)'
-      }"
-      v-model:size="size"
-      min="80px"
-    >
-      <template #first>
-        <a-typography-paragraph>Left</a-typography-paragraph>
-      </template>
-      <template #second>
-        <a-typography-paragraph>Right</a-typography-paragraph>
-      </template>
-    </a-split>
+    <div class="main-container">
+      <TableHeader :show-filter="true" title="xx时间测试用例报告" />
+
+      <TableBody ref="tableBody">
+        <template #header>
+          <div class="text-lg flex-sub">
+            <a-tree blockNode :data="treeData" @select="(key) => click(key[0])" />
+          </div>
+        </template>
+      </TableBody>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { reactive } from 'vue'
 
-const content = ref('')
+function click(key: number) {
+  console.log('被点击了', key)
+}
+
+const treeData = reactive([
+  {
+    title: '新建普通商品并完成购买和售后全流程',
+    key: '0-0',
+    children: [
+      {
+        title: 'web后台登录',
+        key: '0-0-1',
+        children: []
+      },
+      {
+        title: '新建普通商品',
+        key: '0-0-2',
+        children: []
+      },
+      {
+        title: '小程序下单',
+        key: '0-0-3',
+        children: []
+      },
+      {
+        title: '申请售后',
+        key: '0-0-4',
+        children: []
+      }
+    ]
+  },
+  {
+    title: '定金预售下单',
+    key: '0-01',
+    children: [
+      {
+        title: '新建定金预售商品',
+        key: '0-01-0',
+        children: []
+      },
+      {
+        title: '小程序支付',
+        key: '0-01-1',
+        children: []
+      }
+    ]
+  }
+])
 </script>
+<style>
+.flex {
+  display: flex;
+}
+
+.flex-sub {
+  flex: 1;
+}
+</style>
