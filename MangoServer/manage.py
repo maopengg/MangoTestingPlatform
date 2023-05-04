6514c00834672941df989d71e0ce6491bc502bdc
maopeng <729164035@qq.com>
2023-05-04 10:33:50 +0800

    修改了一个函数的执行位置

--- MangoServer/manage.py
+++ MangoServer/manage.py
@@ -2,7 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+
 from script.nuw_logs import __nuw_dir
+
+__nuw_dir()
 
 
 def main():
@@ -20,5 +23,4 @@ def main():
 
 
 if __name__ == '__main__':
-    __nuw_dir()
     main()
