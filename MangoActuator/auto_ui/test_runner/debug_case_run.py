f44fa95564378ff6d4afd11569faf5d437ae21d3
maopeng <729164035@qq.com>
2023-06-13 22:55:31 +0800

    修改UI自动化的继承关系，还有梳理完

--- MangoActuator/auto_ui/test_runner/debug_case_run.py
+++ MangoActuator/auto_ui/test_runner/debug_case_run.py
@@ -4,16 +4,21 @@
 # @Time   : 2023/3/23 11:31
 # @Author : 毛鹏
 
+from typing import Optional
+
 from auto_ui.test_runner.case_run_method import CaseRunMethod
 from auto_ui.ui_tools.base_model import CaseModel
 from utils.decorator.singleton import singleton
 
 
 @singleton
-class CaseDistribution(CaseRunMethod):
+class CaseDistribution:
     """
     用例分发
     """
+
+    def __init__(self):
+        self.run: Optional[CaseRunMethod] = None
 
     async def debug_case_distribution(self, data: CaseModel):
         """
@@ -21,9 +26,15 @@ class CaseDistribution(CaseRunMethod):
         @param data:
         @return:
         """
-        await self.distribute_to_drivers(data)
-        # self.distribute_to_drivers(data)
-        # ResultMain.web_notice(200, '调试用例执行完成，请检查用例执行结果！')
+        if not self.run:
+            with CaseRunMethod() as run:
+                self.run = run
+        await self.run.distribute_to_drivers(data)
+
+    async def test_res_send(self):
+        from utils.socket_client.client_socket import ClientWebSocket
+        client = ClientWebSocket()
+        await client.active_send()
 
     async def close(self):
         await self.web.page.close()
