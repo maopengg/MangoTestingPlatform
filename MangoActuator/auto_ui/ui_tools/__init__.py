67e931bf4794d4b2447562283335272e495ae18a
毛鹏 <729164035@qq.com>
2023-06-29 23:16:23 +0800

    移除执行器项目，开源！

--- MangoActuator/auto_ui/ui_tools/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Project: auto_test
-# @Description: 
-# @Time   : 2023-05-28 18:40
-# @Author : 毛鹏
--- MangoActuator/utils/ope_win/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Project: auto_test
-# @Description: 
-# @Time   : 2023/3/7 11:15
-# @Author : 毛鹏
