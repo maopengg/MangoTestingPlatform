47142d178c7682268650ad8954efdcc1fbb2e92f
毛鹏 <729164035@qq.com>
2023-11-24 11:44:56 +0800

    优化前端websocket链接；优化后端websocket链接改用内存存储对象；增加页面和用例排序功能；前端优化部分字段为空时的判断；增加选择项目和选择环境并保存到后端；优化整体的公共变量的传递问题；删除创建表生成的文件，方便重置项目；等一些列问题！

---
+++ MangoServer/PyAutoTest/auto_test/auto_system/service/socket_link/socket_user.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+# @Project: auto_test
+# @Description: 
+# @Time   : 2023-11-20 16:52
+# @Author : 毛鹏
+from typing import Any
+
+from pydantic import BaseModel
+
+
+class SocketUserModel(BaseModel):
+    user_key: str
+    web_obj: Any | None
+    client_obj: Any | None
+
+
+class SocketUser:
+    user: list[SocketUserModel] = []
+
+    @classmethod
+    def set_user_web_obj(cls, user_key, web_obj):
+        for i in cls.user:
+            if i.user_key == user_key:
+                i.web_obj = web_obj
+                return
+        cls.user.append(SocketUserModel(user_key=user_key, web_obj=web_obj))
+
+    @classmethod
+    def set_user_client_obj(cls, user_key, client_obj):
+        for i in cls.user:
+            if i.user_key == user_key:
+                i.client_obj = client_obj
+                return
+        cls.user.append(SocketUserModel(user_key=user_key, client_obj=client_obj))
+
+    @classmethod
+    def delete_user_web_obj(cls, user_key):
+        for i in cls.user:
+            if i.user_key == user_key:
+                i.web_obj = None
+            if i.client_obj is None and i.web_obj is None:
+                cls.user.remove(i)
+
+    @classmethod
+    def delete_user_client_obj(cls, user_key):
+        for i in cls.user:
+            if i.user_key == user_key:
+                i.client_obj = None
+            if i.client_obj is None and i.web_obj is None:
+                cls.user.remove(i)
+
+    @classmethod
+    def get_user_web_obj(cls, user_key):
+        for i in cls.user:
+            if i.user_key == user_key:
+                return i.web_obj
+
+    @classmethod
+    def get_user_client_obj(cls, user_key):
+        for i in cls.user:
+            if i.user_key == user_key:
+                return i.client_obj
+
+    @classmethod
+    def get_all_user(cls):
+        return [i.user_key for i in cls.user if i.client_obj]
+
+    @classmethod
+    def all_keys(cls):
+        return len(cls.user)
+
+    @classmethod
+    def get_all_user_list(cls):
+        return cls.user
