2957ccedd5a20fa1197e9ff0326e6ba387cc8364
毛鹏 <729164035@qq.com>
2024-01-19 17:59:37 +0800

    优化批量执行用例时的逻辑；修复接口用例响应切换tab页面时的bug；

--- MangoServer/PyAutoTest/auto_test/auto_system/service/socket_link/server_interface_reflection/ui_consumer.py
+++ MangoServer/PyAutoTest/auto_test/auto_system/service/socket_link/server_interface_reflection/ui_consumer.py
@@ -24,6 +24,7 @@ class UIConsumer:
     @convert_args(TestSuiteModel)
     def u_case_batch_result(self, data: TestSuiteModel):
         TestSuiteReportUpdate.update_case_suite_status(data.id, data.status, data.run_status, data.error_message)
-        for i in data.result_list:
-            ConsumerTestResult.update_case_status(i.case_id, data.result_list[0].status)
-            ConsumerTestResult.update_case_result(i, data.error_message)
+        if data.result_list:
+            for i in data.result_list:
+                ConsumerTestResult.update_case_status(i.case_id, data.result_list[0].status)
+                ConsumerTestResult.update_case_result(i, data.error_message)
