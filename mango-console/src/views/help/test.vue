8d307477d206d473ffe00550b5aed422d41b8b56
毛鹏 <729164035@qq.com>
2024-03-06 18:50:45 +0800

    多选输入框

--- mango-console/src/views/help/test.vue
+++ mango-console/src/views/help/test.vue
@@ -2,7 +2,7 @@
   <a-card>
     <div>
       <a-input-tag
-        :default-value="data"
+        :default-value="tags"
         :style="{ width: '380px' }"
         placeholder="Please Enter"
         :max-tag-count="4"
@@ -10,15 +10,25 @@
         @change="handleTagChange"
       />
     </div>
+    <a-form @submit="handleSubmit">
+      <a-form-item>
+        <a-button type="primary" html-type="submit">Submit</a-button>
+      </a-form-item>
+    </a-form>
   </a-card>
 </template>
 
 <script setup lang="ts">
   import { onMounted, reactive } from 'vue'
-  const data: any = reactive(['one', 'two', 'three', 'four'])
+
+  const tags = reactive<string[]>([])
 
   function handleTagChange(value: string[]) {
-    console.log(value)
+    tags.splice(0, tags.length, ...value)
+  }
+
+  function handleSubmit() {
+    console.log('Submitted tags:', tags)
   }
 
   onMounted(() => {})
