16d494084b0d73543013f9d399a48cc5b0ef43ff
毛鹏 <729164035@qq.com>
2024-03-27 12:34:05 +0800

    修改代码的目录和错误日志记录形式

--- MangoActuator/tools/database_tool/mysql_connect.py
+++ MangoActuator/tools/database_tool/mysql_connect.py
@@ -22,7 +22,7 @@ class MysqlConnect:
                 port=mysql_config.port,
                 user=mysql_config.user,
                 password=mysql_config.password,
-                database=mysql_config.db,
+                database=mysql_config.database,
                 autocommit=True
             )
         except OperationalError:
