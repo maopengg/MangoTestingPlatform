267da2ae682c5faf9720c34bf0cfe530b5f55309
毛鹏 <729164035@qq.com>
2024-04-06 22:40:56 +0800

    准备修复多进程中的数据一致性问题；

--- MangoServer/PyAutoTest/exceptions/__init__.py
+++ MangoServer/PyAutoTest/exceptions/__init__.py
@@ -16,7 +16,9 @@ class MangoServerError(Exception):
         if value:
             msg = msg.format(*value)
         if error and is_log:
-            log.error(error)
+            log.error(f'报错提示：{msg}， 报错内容：{error}')
+        else:
+            log.error(f'报错提示：{msg}')
         self.code = code
         self.msg = msg
 
