49cdf917008ab58d492d0206c187857a604d6021
毛鹏 <729164035@qq.com>
2024-04-07 18:04:57 +0800

    修改部分文件和变量的命名规则

--- MangoServer/PyAutoTest/auto_test/auto_api/service/base/http_request.py
+++ MangoServer/PyAutoTest/auto_test/auto_api/service/base/http_request.py
@@ -7,6 +7,7 @@ import logging
 
 import requests
 import time
+from requests import Response
 from requests.exceptions import *
 
 from PyAutoTest.auto_test.auto_system.service.cache_data_value import CacheDataValue
@@ -22,6 +23,7 @@ class HTTPRequest:
 
     def __init__(self):
         self.timeout = CacheDataValue.get_cache_value(CacheDataKeyEnum.API_TIMEOUT.name)
+        # pass
 
     def http(self, request_data: RequestDataModel) -> ResponseDataModel:
         s = time.time()
@@ -65,28 +67,37 @@ class HTTPRequest:
             response_text=response.text
         )
 
+    @classmethod
+    def test_http(cls, request_data: RequestDataModel) -> Response:
+        s = time.time()
+        print(request_data.model_dump_json())
+        response = requests.request(
+            method=request_data.method,
+            url=request_data.url,
+            headers=request_data.headers,
+            params=request_data.params,
+            data=request_data.data,
+            json=request_data.json_data,
+            files=request_data.file,
+            timeout=int(30)
+        )
+        end = time.time() - s
+        return response
+
 
 if __name__ == '__main__':
-    data = {"method": "POST", "url": "https://app-test.growknows.cn/dev-api/business/template/add", "headers": {
-        "Authorization": "eyJhbGciOiJIUzUxMiJ9.eyJhY2Nlc3NfdG9rZW4iOiJCZWFyZXIgdG9rZW46bG9naW5fdG9rZW5zOjE6MTphY2Nlc3NfdG9rZW46OWExNDdlMTMtYjYzMS00OGM1LWE4N2MtMTIyYWIyNDA5ZDI3IiwicmVmcmVzaF90b2tlbiI6IkJlYXJlciB0b2tlbjpsb2dpbl90b2tlbnM6MToxOnJlZnJlc2hfdG9rZW46eXg5OWNHbXBpVm5ucTdWQ1h1SFFiVmI5WmZaVVh5RVRxdjhLZ0htVnpSTk95WlpJV1lNVmltaFVwRHdSdnN0Y2hScTMyMGJoTHdueXNVMUhRVHFNUGZaSFdHNzc0OVNSeTBfS29haHptdzlXaWI2MlBOcFBFeHBXNTZHdHNBM1QiLCJhY2NvdW50X3R5cGUiOiJhZG1pbiIsInVzZXJfdHlwZSI6IjAwIiwidXNlcl9pZCI6MSwidXNlcl9uYW1lIjoiYWRtaW5AYWlnYy5jb20iLCJpc19sZXNzb3IiOiJZIiwiZW50ZXJwcmlzZV9pZCI6MSwiZW50ZXJwcmlzZV9uYW1lIjoiYWRtaW5pc3RyYXRvciIsInNvdXJjZV9uYW1lIjoic2xhdmUifQ.OcIo2ydTAlt8735-B5Eaw4fOb5meuS93O9ggqdyl96HLlsw1yWa-ZORNfu5lHjB3d_N8rvxGIUd5Uk6JfBTVyg",
-        "Accept": "application/json, text/plain, */*", "Content-Type": "application/json"}, "params": None,
-            "data": None, "json_data": {"template": {
-            "logo": "https://minio-test.growknows.cn/aigc-dev/2024/01/23/主图-06_bcc2aaf54a754f6699aa72cecae3f553.jpg",
-            "name": "api自动化测试模版", "type": "1", "detail": None, "summary": "api自动化测试模版", "categoryId": "1",
-            "screenshot": None}, "templateFlowInfo": {"workFlowIds": None, "fromInputArr": [], "connectOutputArr": []}},
-            "file": None}
+    data = {"method": "POST",
+            "url": "https://sara-test.growknows.cn/dev-api/aigc-order/goodsCouponsInfo/addGoodsCoupons",
+            "headers": {
+                "Authorization": "eyJhbGciOiJIUzUxMiJ9.eyJhY2Nlc3NfdG9rZW4iOiJCZWFyZXIgdG9rZW46bG9naW5fdG9rZW5zOjE6MjphY2Nlc3NfdG9rZW46NzJlNjEwNTgtNmY3NS00MDg5LTg4N2YtZWMyM2RkNmY4MTJjIiwicmVmcmVzaF90b2tlbiI6IkJlYXJlciB0b2tlbjpsb2dpbl90b2tlbnM6MToyOnJlZnJlc2hfdG9rZW46SC02SGRraDBDckkyUXUwSHlXelYybFM2YzlYenZLamZKQzlBMkU5NnJsWjgzQUhkTTRrbjlvZGhUNmVXU1paUDI2dHc3MEZLREhDR29xMWxtY3cxeTh2clpxV0lzU2F0MVRPSXZERHhIMXdCaUVIalBIYlpRRU04VjdQODdzcTciLCJhY2NvdW50X3R5cGUiOiJ2aXAiLCJ1c2VyX3R5cGUiOiIwMCIsInVzZXJfaWQiOjIsInVzZXJfbmFtZSI6ImFkbWluQGFpZ2MuY29tIiwiaXNfbGVzc29yIjoiWSIsImVudGVycHJpc2VfaWQiOjEsImVudGVycHJpc2VfbmFtZSI6ImFkbWluaXN0cmF0b3IiLCJzb3VyY2VfbmFtZSI6InNsYXZlIn0.h9VybBPjJVq5jZQFUhrWQ0vvwHP83x9yhlG0727p9_QywlBlWIhXtdnMFObysPUfuMMxX9lX1DPgk8Gbn6Qcpw",
+                "content-type": "application/json"
 
-    print(HTTPRequest().http(RequestDataModel(**data)).response_text)
-"""
-data 和 json 是 requests 库中用于发送 HTTP 请求体数据的两个参数。它们的最大区别在于数据格式和发送方式。
+            },
+            "params": None,
+            "data": None,
+            "json_data": {"name": "自动化测试001", "expDate": "2024-04-04 00:00:00", "expType": "2",
+                          "startDate": "2024-04-01 00:00:00", "couponsNum": 1, "couponsType": "1", "targetUsers": "",
+                          "couponsRulesJson": {"frequency": 2, "exchangeCycle": "everyDay"},
+                          "goodsDiscountsRuleIds": "1775381374142443522"}, "file": None}
 
-data 参数用于发送表单数据或者文件数据，它的值可以是一个字典对象或者一个文件对象。当请求体数据是表单数据时，
-data 参数应该是一个字典对象，其中键值对表示表单字段和字段值。当请求体数据是文件数据时，data 参数应该是一个
-文件对象，例如使用 open() 函数打开的文件对象。
-
-json 参数用于发送 JSON 数据，它的值应该是一个 Python 对象。requests 库会自动将 Python 对象序列化为 
-JSON 格式的字符串，并设置请求头的 Content-Type 为 application/json。服务器收到请求后会自动将 JSO
-N 字符串反序列化为对应的数据类型。
-
-因此，最大的区别在于数据格式和发送方式。如果需要发送表单数据或者文件数据，则应该使用 data 参数；如果需要发送 JSON 数据，则应该使用 json 参数。
-"""
+    print(HTTPRequest.test_http(RequestDataModel(**data)).json())
