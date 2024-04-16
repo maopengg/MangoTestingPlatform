343f1e923c8f44d1dc151c5080e822779a7c11c0
毛鹏 <729164035@qq.com>
2024-04-16 15:34:24 +0800

    解决数据库中重复的key的问题

--- MangoServer/PyAutoTest/auto_test/auto_system/service/socket_link/server_interface_reflection/system_consumer.py
+++ MangoServer/PyAutoTest/auto_test/auto_system/service/socket_link/server_interface_reflection/system_consumer.py
@@ -5,7 +5,8 @@
 # @Author : 毛鹏
 import logging
 
-from PyAutoTest.auto_test.auto_system.views.cache_data import CacheDataCRUD
+from PyAutoTest.auto_test.auto_system.models import CacheData
+from PyAutoTest.auto_test.auto_system.views.cache_data import CacheDataSerializers, CacheDataCRUD
 from PyAutoTest.enums.system_enum import CacheValueTypeEnum
 
 log = logging.getLogger('system')
@@ -18,10 +19,14 @@ class SystemConsumer:
         # redis = RedisBase('default')
         for i in data:
             for key, value in i.items():
-                CacheDataCRUD().inside_post({
-                    'describe': key,
-                    'key': key,
-                    'value': value,
-                    'value_type': CacheValueTypeEnum.DICT.value,
-                })
-                # redis.set_key(key, value)
+                if not CacheData.objects.filter(key=key):
+                    serializer = CacheDataSerializers(data={
+                        'describe': key,
+                        'key': key,
+                        'value': value,
+                        'value_type': CacheValueTypeEnum.DICT.value,
+                    })
+                    if serializer.is_valid():
+                        serializer.save()
+                    else:
+                        log.error(f'执行修改时报错，请检查！数据：{data}, 报错信息：{str(serializer.errors)}')
