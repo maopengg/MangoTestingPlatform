0113c5c14f51cec99bcfa78cc6c1532cdce45347
毛鹏 <729164035@qq.com>
2024-04-19 18:37:58 +0800

    删除部分注释

--- MangoServer/PyAutoTest/auto_test/auto_api/service/write_api.py
+++ MangoServer/PyAutoTest/auto_test/auto_api/service/write_api.py
@@ -6,28 +6,20 @@
 import logging
 
 from PyAutoTest.auto_test.auto_api.models import ApiInfo
-from PyAutoTest.auto_test.auto_api.views.api_info import ApiInfoSerializers
+from PyAutoTest.auto_test.auto_api.views.api_info import ApiInfoCRUD
 from PyAutoTest.models.socket_model.api_model import ApiInfoModel
 
 log = logging.getLogger('api')
 
 
 class WriteAPI:
-    def __init__(self):
-        self.api_info_list: list = [str(method) + url for url, method in
-                                    ApiInfo.objects.all().values_list('url', 'method')]
 
-    def write(self, data: ApiInfoModel):
+    @classmethod
+    def write(cls, data: ApiInfoModel):
         data = data.dict()
         data['json'] = data['json_data']
         del data['json_data']
-        check = str(data['method']) + data['url']
-        if check not in self.api_info_list:
-            serializers = ApiInfoSerializers(data=data)
-            if serializers.is_valid():
-                serializers.save()
-                self.api_info_list.append(check)
-            else:
-                log.error(str(serializers.errors))
-        else:
-            log.info(f'url:{data["url"]}是重复的数据，跳过该接口信息的保存！')
+        try:
+            api_info_obj = ApiInfo.objects.get(url=data['url'], method=data['method'])
+        except ApiInfo.DoesNotExist:
+            ApiInfoCRUD.inside_post(data)
