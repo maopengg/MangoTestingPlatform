17de798bc611946fbf9e305826cde46f17994e88
毛鹏 <729164035@qq.com>
2024-04-23 18:25:00 +0800

    更新菜单的url

--- mango-console/src/api/url.ts
+++ mango-console/src/api/url.ts
@@ -10,7 +10,7 @@ export const getCommentList = '/getCommentList'
 
 // 修改原始接口后的
 export const login = '/login'
-export const getMenuListByRoleId = '/menu/'
+export const getMenuListByRoleId = '/menu'
 export const getAllMenuByRoleId = '/role'
 export const getMenuList = '/menu_1'
 export const test = '/config/test'
