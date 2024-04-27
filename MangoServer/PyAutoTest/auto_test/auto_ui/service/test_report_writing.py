c8c00a7fbb1a918899e0b835fd5857aa63629e0c
毛鹏 <729164035@qq.com>
2024-04-27 18:48:12 +0800

    修复一些bug；并且增加了一些用例；

--- MangoServer/PyAutoTest/auto_test/auto_ui/service/test_report_writing.py
+++ MangoServer/PyAutoTest/auto_test/auto_ui/service/test_report_writing.py
@@ -89,7 +89,8 @@ class TestReportWriting:
                 test_suite_obj.status = StatusEnum.SUCCESS.value
             test_suite_obj.error_message = json.dumps(error_message_list, ensure_ascii=False)
             test_suite_obj.save()
-            NoticeMain.notice_main(test_suite_obj.project.id, test_suite_id)
+            if test_suite_obj.is_notice:
+                NoticeMain.notice_main(test_suite_obj.project.id, test_suite_id)
             from PyAutoTest.auto_test.auto_system.consumers import ChatConsumer
             ChatConsumer.active_send(SocketDataModel(
                 code=code,
