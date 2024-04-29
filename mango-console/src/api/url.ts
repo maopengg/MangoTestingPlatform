41071e6de53777ebdc00d15a0a5bd0f184ccbbe8
毛鹏 <729164035@qq.com>
2024-04-29 11:07:30 +0800

    增加新增用户功能；

--- mango-console/src/api/url.ts
+++ mango-console/src/api/url.ts
@@ -10,6 +10,7 @@ export const getCommentList = '/getCommentList'
 
 // 修改原始接口后的
 export const login = '/login'
+export const register = '/register'
 export const getMenuListByRoleId = '/menu'
 export const getAllMenuByRoleId = '/role'
 export const getMenuList = '/menu_1'
