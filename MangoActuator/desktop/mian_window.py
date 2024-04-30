848c838cd43c971de7aba9c6fa7cd9c85b187816
毛鹏 <729164035@qq.com>
2024-04-30 18:36:59 +0800

    优化了执行器报错；

--- MangoActuator/desktop/mian_window.py
+++ MangoActuator/desktop/mian_window.py
@@ -51,7 +51,7 @@ class MainWindow(QMainWindow, Window):
     def __init__(self):
         super().__init__()
         self.setup()
-        self.resize(1250, 800)
+        self.resize(1000, 640)
 
         # 创建系统托盘图标
         self.tray_icon = QSystemTrayIcon(self)
