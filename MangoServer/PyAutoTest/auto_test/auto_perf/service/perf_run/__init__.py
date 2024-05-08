443807bf353773c24fda64a3d554e745eb0fb017
毛鹏 <729164035@qq.com>
2024-05-08 17:54:01 +0800

    平台集成性能测试的探索

---
+++ MangoServer/PyAutoTest/auto_test/auto_perf/service/perf_run/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Project: auto_test
+# @Description: 
+# @Time   : 2024-05-08 17:12
+# @Author : 毛鹏
--- MangoServer/PyAutoTest/auto_test/auto_perf/urls.py
+++ MangoServer/PyAutoTest/auto_test/auto_perf/urls.py
@@ -3,12 +3,14 @@
 # @Description:  性能测试子路由
 # @Time   : 2023-01-19 19:21
 # @Author : 毛鹏
+from django.urls import path
 from rest_framework import routers
+
+from PyAutoTest.auto_test.auto_perf.views.perf import PerfViews
 
 routers = routers.DefaultRouter()
 urlpatterns = [
-    # 性能测试-->页面返回接口
-    # path("data/", perf_data),
+    #
+    path("run", PerfViews.as_view({'post': 'perf_run'})),
     # path("report/", perf_report),
 ]
-urlpatterns += routers.urls
