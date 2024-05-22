5729474e0b65c5854fe86de99e44a5c6ac5e5835
毛鹏 <729164035@qq.com>
2024-05-22 17:59:28 +0800

    测试代码

--- MangoActuator/tools/data_processor/__init__.py
+++ MangoActuator/tools/data_processor/__init__.py
@@ -151,7 +151,13 @@ class DataProcessor(ObtainRandomData, DataClean):
 
 
 if __name__ == '__main__':
+    # 把变量进行使用，并且存到环境中
     str_ = "${goods_name_int(),flow名称}"
     r = DataProcessor()
     print(r.replace(str_))
     print(r.get_cache('flow名称'))
+    # 多层嵌套
+    str_ = "${姓名：${name}}"
+    r.set_cache('name', 'maopeng')
+    r.set_cache('姓名：maopeng', '27')
+    print(r.replace(str_))
