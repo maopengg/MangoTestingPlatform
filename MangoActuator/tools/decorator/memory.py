b3f6ceee4ebef6b3cf685fdc409e89c1d9bc28d2
毛鹏 <729164035@qq.com>
2024-05-24 14:31:27 +0800

    优化执行器对于内存的控制，减少崩溃的问题产生；

---
+++ MangoActuator/tools/decorator/memory.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+# @Project: auto_test
+# @Description: 
+# @Time   : 2024-05-24 11:51
+# @Author : 毛鹏
+import asyncio
+import time
+
+import psutil
+
+from tools.desktop.signal_send import SignalSend
+
+MEMORY_THRESHOLD = 80
+
+LOOP_MIX = 10
+
+
+def async_memory(func):
+    async def wrapper(*args, **kwargs):
+        current_mix = 0
+        while True:
+            memory_percent = psutil.virtual_memory().percent
+            if memory_percent > MEMORY_THRESHOLD:
+                # log.info(f'当前的内存使用率不足以支持继续启动浏览器，请稍等内存减少后继续，当前次数：{current_mix}')
+                await asyncio.sleep(3)
+                current_mix += 1
+                if current_mix == 10:
+                    SignalSend.notice_signal_c(f'程序占用内存过多，请减少并发浏览器的数量，或者检查电脑是否有满足执行自动化任务的内存空间！')
+            else:
+                break
+            if current_mix >= LOOP_MIX:
+                break
+        return await func(*args, **kwargs)
+
+    return wrapper
+
+
+def sync_memory(func):
+    def wrapper(*args, **kwargs):
+        current_mix = 0
+        while True:
+            memory_percent = psutil.virtual_memory().percent
+            if memory_percent > MEMORY_THRESHOLD:
+                # log.info(f'当前的内存使用率不足以支持继续启动浏览器，请稍等内存减少后继续，当前次数：{current_mix}')
+                time.sleep(3)
+                current_mix += 1
+                if current_mix == 10:
+                    SignalSend.notice_signal_c(f'程序占用内存过多，请减少并发浏览器的数量，或者检查电脑是否有满足执行自动化任务的内存空间！')
+            else:
+                break
+            if current_mix > LOOP_MIX:
+                break
+        return func(*args, **kwargs)
+
+    return wrapper
