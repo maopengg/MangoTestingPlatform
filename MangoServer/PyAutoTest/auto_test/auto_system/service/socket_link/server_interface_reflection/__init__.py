ff758120a7d984da807612fd56782014b254717d
毛鹏 <729164035@qq.com>
2024-05-27 14:12:11 +0800

    优化日志打印逻辑

--- MangoServer/PyAutoTest/auto_test/auto_system/service/socket_link/server_interface_reflection/__init__.py
+++ MangoServer/PyAutoTest/auto_test/auto_system/service/socket_link/server_interface_reflection/__init__.py
@@ -16,6 +16,7 @@ from PyAutoTest.auto_test.auto_system.service.socket_link.server_interface_refle
     SystemConsumer
 from PyAutoTest.auto_test.auto_system.service.socket_link.server_interface_reflection.ui_consumer import UIConsumer
 from PyAutoTest.models.socket_model import QueueModel
+from PyAutoTest.settings import DEBUG
 from PyAutoTest.tools.decorator.singleton import singleton
 
 log = logging.getLogger('system')
@@ -33,7 +34,8 @@ class ServerInterfaceReflection(APIConsumer, SystemConsumer, UIConsumer, PerfCon
         if sender == "websocket":
             data = kwargs.get('data')
             if isinstance(data, QueueModel):
-                log.info(f"开始处理接收的消息：{data.model_dump_json()}")
+                if DEBUG:
+                    log.info(f"开始处理接收的消息：{data.model_dump_json()}")
                 future = self.executor.submit(getattr(self, data.func_name), data.func_args)
                 future.add_done_callback(self.handle_task_result)  # 添加任务完成后的回调函数
             else:
