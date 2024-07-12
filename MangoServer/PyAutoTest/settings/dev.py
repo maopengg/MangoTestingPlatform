3378e20712cde7f0711f1bb20980327a70311810
毛鹏 <729164035@qq.com>
2024-07-12 17:42:23 +0800

    增加后端版本控制；修复iframe的问题

---
+++ MangoServer/PyAutoTest/settings/dev.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# @Project: auto_test
+# @Description:
+# @Time   : 2024-07-12 14:13
+# @Author : 毛鹏
+
+# ************************ 数据源类型 ************************ #
+
+IS_SQLITE = False  # 是否选用mysql作为数据源
+
+# ************************ Mysql配置 ************************ #
+
+mysql_db_name = 'mango_server_test'
+# mysql_db_name = 'mango_server'
+mysql_user = 'root'
+mysql_password = 'mP123456&'
+mysql_ip = '127.0.0.1'
+# mysql_ip = '121.37.174.56'
+mysql_port = 3306
+
+# ************************ DEBUG配置 ************************ #
+DEBUG = True
+
+# ************************ REDIS配置 ************************ #
+
+REDIS = False
--- MangoServer/script/mango-console-script.sh
+++
@@ -1,5 +0,0 @@
-rm -rf /root/uitest
-git clone -b master http://aigcdeploy:aigcdeploy@git.zalldigital.cn/z-test/uitest.git
-cd /root/uitest/mango-console/
-npm i
-npm run build
