599036da69e7e749b9f540bcfac0e02546591332
毛鹏 <729164035@qq.com>
2024-07-15 18:13:27 +0800

    增加一个项目禁止删除的权限，防止我的线上环境被人删除数据；

--- MangoServer/PyAutoTest/middleware/is_delete.py
+++ MangoServer/PyAutoTest/middleware/is_delete.py
@@ -15,11 +15,10 @@ class IsDeleteMiddleWare(MiddlewareMixin):
 
     def process_request(self, request: ASGIRequest):
         if not IS_DELETE:
-            print(request.user)
             if request.method == 'DELETE':
                 return JsonResponse({
                     "code": 300,
-                    "msg": "演示项目非管理员权限禁止删除",
+                    "msg": "演示环境非管理员权限禁止删除",
                     "data": None
                 }, status=200)
 
