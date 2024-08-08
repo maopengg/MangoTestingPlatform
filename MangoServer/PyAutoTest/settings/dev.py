89691da0f7921e6e55bdf82dec4a34d25c4eda34
毛鹏 <729164035@qq.com>
2024-08-08 11:00:40 +0800

    提交

--- MangoServer/PyAutoTest/settings/dev.py
+++ MangoServer/PyAutoTest/settings/dev.py
@@ -3,6 +3,8 @@
 # @Description:
 # @Time   : 2024-07-12 14:13
 # @Author : 毛鹏
+import json
+import os
 
 # ************************ 数据源类型 ************************ #
 
@@ -15,6 +17,19 @@ mysql_user = 'root'
 mysql_password = 'mP123456&'
 mysql_ip = '127.0.0.1'
 
+# 个人配置，开源用户忽略这部分代码
+file_name = 'PyAutoTest/settings/database.json'
+if os.path.exists(file_name):
+    # 读取数据
+    with open(file_name, 'r') as file:
+        data = json.load(file)
+
+    mysql_port = data.get('mysql_port', mysql_port)
+    mysql_db_name = data.get('mysql_db_name', mysql_db_name)
+    mysql_user = data.get('mysql_user', mysql_user)
+    mysql_password = data.get('mysql_password', mysql_password)
+    mysql_ip = data.get('mysql_ip', mysql_ip)
+
 # ************************ DEBUG配置 ************************ #
 DEBUG = True
 
@@ -24,4 +39,3 @@ REDIS = False
 
 # ************************ 是否允许删除 ************************ #
 IS_DELETE = True
-
