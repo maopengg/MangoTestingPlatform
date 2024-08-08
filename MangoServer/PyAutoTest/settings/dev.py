e4017c9cc8234a55f1375857350b4b3cf96974b5
毛鹏 <729164035@qq.com>
2024-08-08 10:28:31 +0800

    提交

--- MangoServer/PyAutoTest/settings/dev.py
+++ MangoServer/PyAutoTest/settings/dev.py
@@ -10,14 +10,11 @@ IS_SQLITE = False  # 是否选用mysql作为数据源
 
 # ************************ Mysql配置 ************************ #
 mysql_port = 3306
-# mysql_db_name = 'test'
-# mysql_user = 'root'
-# mysql_password = 'mP123456&'
-# mysql_ip = '127.0.0.1'
-mysql_db_name = 'auto_test'
+mysql_db_name = 'test'
 mysql_user = 'root'
-mysql_password = 'Root@123'
-mysql_ip = '172.16.100.26'
+mysql_password = 'mP123456&'
+mysql_ip = '127.0.0.1'
+
 # ************************ DEBUG配置 ************************ #
 DEBUG = True
 
