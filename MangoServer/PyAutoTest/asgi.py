7899a5891b78ad8bd891a133cc50b86717607873
毛鹏 <729164035@qq.com>
2024-09-09 18:28:01 +0800

    提交

--- MangoServer/PyAutoTest/asgi.py
+++ MangoServer/PyAutoTest/asgi.py
@@ -10,13 +10,28 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
+import sys
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from django.core.management import call_command
 
 from . import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PyAutoTest.settings')
+
+# 添加项目路径到系统路径
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+
+# 检查并执行迁移
+def migrate():
+    from django import setup
+    setup()
+    call_command('migrate')
+
+
+migrate()
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
