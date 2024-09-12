ac6dc20d00759e8f8309ed262d143c1b38e0c5cd
毛鹏 <729164035@qq.com>
2024-09-12 10:07:07 +0800

    编写工作的脚本

--- MangoActuator/tests/page_test.py
+++ MangoActuator/tests/page_test.py
@@ -1,65 +1,90 @@
-from PySide6.QtWidgets import QApplication, QTableWidget, QVBoxLayout, QWidget
+import sys
+from PySide6.QtWidgets import QComboBox, QMainWindow, QApplication, QToolTip, QLineEdit
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QFont
 
-class MangoTableWidget(QTableWidget):
-    style = """
-    QTableWidget {
-        background-color: white;
-        padding: 5px;
-        border-radius: 8px;
-        gridline-color: gray;
-        color: black;
-    }
-    QTableWidget::item {
-        border-color: none;
-        padding-left: 5px;
-        padding-right: 5px;
-        border-bottom: 1px solid lightgray;
-    }
-    QTableWidget::item:selected {
-        background-color: lightblue;
-    }
-    QHeaderView::section {
-        background-color: gray;
-        border: 1px solid darkgray;
-    }
-    """
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class CheckableComboBox(QComboBox):
+    def __init__(self, parent=None):
+        super(CheckableComboBox, self).__init__(parent)
+        QToolTip.setFont(QFont('Times New Roman', 15))  # 设置提示框字体和字号
+        self.setLineEdit(QLineEdit())
+        self.lineEdit().setReadOnly(True)
+        self.view().clicked.connect(self.selectItemAction)
+        self.addCheckableItem('全选')
+        self.SelectAllStatus = 1
 
-        # 设置列数
-        self.setColumnCount(8)
+    def addCheckableItem(self, text):
+        super().addItem(text)
+        item = self.model().item(self.count() - 1, 0)
+        item.setFlags(Qt.ItemFlag.ItemIsUserCheckable | Qt.ItemFlag.ItemIsEnabled)
+        item.setCheckState(Qt.CheckState.Unchecked)
+        item.setToolTip(text)
 
-        # 设置样式
-        self.setStyleSheet(self.style)
+    def addCheckableItems(self, texts):
+        for text in texts:
+            self.addCheckableItem(text)
 
-        # 隐藏行头
-        self.verticalHeader().setVisible(False)
+    def ifChecked(self, index):
+        item = self.model().item(index, 0)
+        return item.checkState() == Qt.CheckState.Checked
 
-        # 设置列宽
-        self.set_column_width()
+    def checkedItems(self):
+        return [self.itemText(i) for i in range(self.count()) if self.ifChecked(i)]
 
-    def set_column_width(self):
-        print("Setting column widths")
-        self.setColumnWidth(0, 100)
-        self.setColumnWidth(1, 200)
-        self.setColumnWidth(2, 300)
-        self.setColumnWidth(3, 50)
-        self.setColumnWidth(4, 50)
-        self.setColumnWidth(5, 50)
-        self.setColumnWidth(6, 50)
-        self.setColumnWidth(7, 50)
+    def checkedItemsStr(self):
+        return ';'.join(self.checkedItems()).strip('全选').strip(';')
 
-class SomeOtherClass(QWidget):
+    def showPopup(self):
+        self.view().setMinimumWidth(3 * self.width() // 2)  # 下拉列表宽度加大
+        self.view().setMaximumHeight(200)  # 最大高度为200
+        super().showPopup()
+
+    def selectItemAction(self, index):
+        if index.row() == 0:
+            for i in range(self.model().rowCount()):
+                if self.SelectAllStatus:
+                    self.model().item(i).setCheckState(Qt.CheckState.Checked)
+                else:
+                    self.model().item(i).setCheckState(Qt.CheckState.Unchecked)
+            self.SelectAllStatus = (self.SelectAllStatus + 1) % 2
+
+        self.lineEdit().clear()
+        self.lineEdit().setText(self.checkedItemsStr())
+
+    def clear(self) -> None:
+        super().clear()
+        self.addCheckableItem('全选')
+
+    def select_all(self):
+        for i in range(self.model().rowCount()):
+            self.model().item(i).setCheckState(Qt.CheckState.Checked)
+        self.lineEdit().setText(self.checkedItemsStr())
+
+
+class Ui_Study(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.table = MangoTableWidget()
-        layout = QVBoxLayout(self)
-        layout.addWidget(self.table)
-        self.setLayout(layout)
+        self.resize(500, 500)
+        self.setMinimumSize(500, 500)
 
-if __name__ == "__main__":
-    app = QApplication([])
-    window = SomeOtherClass()
-    window.show()
-    app.exec()
+        self.combobox = CheckableComboBox(self)
+        self.combobox.move(20, 20)
+        self.combobox.resize(200, 30)
+
+        self.line_edit = QLineEdit(self)
+        self.line_edit.move(20, 80)
+        self.line_edit.resize(300, 30)
+
+        self.combobox.addCheckableItems(['sdsfdsf', 'hgjghj', 'reyeyey'])
+        self.combobox.setCurrentIndex(-1)
+        self.combobox.lineEdit().textChanged.connect(lambda: self.line_edit.setText(self.combobox.checkedItemsStr()))
+
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    my_ui = Ui_Study()
+    a = my_ui.combobox
+    sys.exit(app.exec())
--- MangoActuator/tests/test_widget.py
+++ MangoActuator/tests/test_widget.py
@@ -5,52 +5,18 @@
 # @Author : 毛鹏
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QApplication
 
+from src.enums.tools_enum import StatusEnum
+from src.models.gui_model import ComboBoxDataModel
 from src.widgets import *
 
 
 class MyQWidget(QWidget):
-    data = {"title": "项目/产品", "placeholder": "请选择项目产品", "key": "project_product", "input": None,
-            "text": None, "type": 2, "select": [{"value": 3, "label": "演示项目",
-                                                 "children": [{"value": 13, "label": "测试项目百度", "children": []},
-                                                              {"value": 12, "label": "多少度", "children": []},
-                                                              {"value": 11, "label": "arco（组件库）",
-                                                               "children": [{"value": 35, "label": "气泡确认框"},
-                                                                            {"value": 34, "label": "通知提醒框"},
-                                                                            {"value": 33, "label": "对话框"},
-                                                                            {"value": 32, "label": "全局提示"},
-                                                                            {"value": 31, "label": "上传"},
-                                                                            {"value": 30, "label": "树选择"},
-                                                                            {"value": 29, "label": "数据穿梭框"},
-                                                                            {"value": 28, "label": "时间选择器"},
-                                                                            {"value": 27, "label": "文本域"},
-                                                                            {"value": 26, "label": "开关"},
-                                                                            {"value": 25, "label": "滑动输入条"},
-                                                                            {"value": 24, "label": "选择器"},
-                                                                            {"value": 23, "label": "评分"},
-                                                                            {"value": 22, "label": "单选框"},
-                                                                            {"value": 21, "label": "标签输入框"},
-                                                                            {"value": 20, "label": "验证码输入框"},
-                                                                            {"value": 19, "label": "数字输入框"},
-                                                                            {"value": 18, "label": "输入框"},
-                                                                            {"value": 17, "label": "表单"},
-                                                                            {"value": 16, "label": "日期选择器"},
-                                                                            {"value": 15, "label": "颜色选择器"},
-                                                                            {"value": 14, "label": "复选框"},
-                                                                            {"value": 13, "label": "级联选择"},
-                                                                            {"value": 12, "label": "自动补全"},
-                                                                            {"value": 11, "label": "首页title"}]},
-                                                              {"value": 3, "label": "百度",
-                                                               "children": [{"value": 3, "label": "首页"}]},
-                                                              {"value": 2, "label": "微信",
-                                                               "children": [{"value": 2, "label": "首页"}]},
-                                                              {"value": 1, "label": "玩安卓",
-                                                               "children": [{"value": 1, "label": "首页"}]}]}]}
-
     def __init__(self):
         super().__init__()
         self.layout = QVBoxLayout()
         self.setFixedSize(500, 350)
-        combo_box = MangoCascader(self.data['placeholder'], self.data['select'])
+        data: list[ComboBoxDataModel] = StatusEnum.get_select()
+        combo_box = MangoComboBox('请选择状态', data)
         # 添加选项
 
         # combo_box.setWindowIcon(QIcon(':/icons/icons/down.svg'))
