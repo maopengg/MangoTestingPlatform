d43abc19da00291affa28412adeba9ff0912be29
毛鹏 <729164035@qq.com>
2024-09-18 11:47:53 +0800

    修改首页的样式

--- MangoActuator/main.py
+++ MangoActuator/main.py
@@ -20,10 +20,5 @@ except Exception as error:
     traceback.print_exc()
     log.error(f"顶级任务出现异常：{error}")
 
-# pyinstaller -F -c .\main.py --upx-dir="E:\DevTool\Python\python-venv\upx-4.0.2-win64"
-# pyinstaller -F -w --name=执行器 --icon=resources/icons/app_icon.png --add-data "resources/icons/app_icon.png;resources/icons" .\main.py
-# pyinstaller -F -w  .\main.py
-# playwright install ffmpeg
-# pyside6-rcc resources.qrc -o resources/icons/app_rc.py
 # pyinstaller -F -w --onefile --add-data "src/settings/settings.json;src/settings" --add-data "src/settings/menus.json;src/settings" --add-data "src/settings/mango.json;src/settings" --name=执行器 --icon=resources/icons/app_icon.png main.py
 
