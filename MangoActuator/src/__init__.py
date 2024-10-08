d3784166672f5b5dec5a87afa8bf7ea5e65a0bf2
毛鹏 <729164035@qq.com>
2024-10-08 21:12:25 +0800

    修改目录

--- MangoActuator/src/__init__.py
+++ MangoActuator/src/__init__.py
@@ -7,7 +7,7 @@ from PySide6.QtCore import *
 from PySide6.QtGui import *
 from PySide6.QtWidgets import *
 from PySide6.QtSvgWidgets import *
-from resources.app_rc import *
+# from resources.app_rc import *
 from src.settings.settings import *
 from src.settings import settings
 from src.network.web_socket.socket_api_enum import *
