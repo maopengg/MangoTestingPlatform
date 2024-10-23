bf62146e9882c3b31b5c632756b90fe59a19d555
毛鹏 <729164035@qq.com>
2024-10-23 15:50:11 +0800

    继续优化；

--- MangoActuator/src/pages/home/home.py
+++ MangoActuator/src/pages/home/home.py
@@ -19,14 +19,20 @@ class HomePage(QWidget):
 
         self.label_6 = MangoLabel(f'欢迎{UserModel().nickname}使用芒果测试平台！', self)
         self.layout.addWidget(self.label_6)
-        mango_dialog = MangoDialog('添加作者微信，然后拉你进平台交流群')
+        self.mango_dialog = MangoDialog('添加作者微信进芒果测试平台交流群', (260, 340))
         label = QLabel()
-        pixmap = QPixmap(":/icons/app_icon.png")  # 替换为你的图片路径
+        pixmap = QPixmap(":/picture/author.png")  # 替换为你的图片路径
         label.setPixmap(pixmap)
-        mango_dialog.layout.addWidget(label)
-        mango_dialog.exec()
+        label.setScaledContents(True)  # 允许缩放
+        label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)  # 设置大小策略
+        self.mango_dialog.layout.addWidget(label)
+
+
     def show_data(self):
-        pass
+        QTimer.singleShot(1000, self.open_dialog)  # 1000毫秒后调用open_dialog方法
+
+    def open_dialog(self):
+        self.mango_dialog.exec()
 
     def signal_label_6(self, text):
         self.label_6.setText(text)
