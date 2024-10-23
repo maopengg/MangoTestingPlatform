3ca7f16d343bc7f12dfd4c0fb85bade9e0abda6d
毛鹏 <729164035@qq.com>
2024-10-23 15:35:23 +0800

    继续优化；

--- MangoActuator/src/pages/home/home.py
+++ MangoActuator/src/pages/home/home.py
@@ -6,7 +6,9 @@
 
 from mango_ui import *
 from mango_ui.init import *
+
 from src.models.user_model import UserModel
+
 
 class HomePage(QWidget):
     def __init__(self, parent=None):
@@ -17,7 +19,12 @@ class HomePage(QWidget):
 
         self.label_6 = MangoLabel(f'欢迎{UserModel().nickname}使用芒果测试平台！', self)
         self.layout.addWidget(self.label_6)
-
+        mango_dialog = MangoDialog('添加作者微信，然后拉你进平台交流群')
+        label = QLabel()
+        pixmap = QPixmap(":/icons/app_icon.png")  # 替换为你的图片路径
+        label.setPixmap(pixmap)
+        mango_dialog.layout.addWidget(label)
+        mango_dialog.exec()
     def show_data(self):
         pass
 
