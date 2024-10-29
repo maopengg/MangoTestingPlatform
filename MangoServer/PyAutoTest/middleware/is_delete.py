c403dfd14751abaeca507209841f9a2c26229f86
毛鹏 <729164035@qq.com>
2024-10-29 17:18:33 +0800

    增加了一个哥们的put权限

--- MangoServer/PyAutoTest/middleware/is_delete.py
+++ MangoServer/PyAutoTest/middleware/is_delete.py
@@ -3,16 +3,14 @@
 # @Description: 
 # @Time   : 2024-07-15 17:48
 # @Author : 毛鹏
-import time
 
 import jwt
+from django.conf import settings
 from django.core.handlers.asgi import ASGIRequest
 from django.http import JsonResponse
 from django.utils.deprecation import MiddlewareMixin
 
 from PyAutoTest.settings import IS_DELETE
-import jwt
-from django.conf import settings
 
 
 class IsDeleteMiddleWare(MiddlewareMixin):
@@ -21,8 +19,8 @@ class IsDeleteMiddleWare(MiddlewareMixin):
         if not IS_DELETE:
             token = request.META.get('HTTP_AUTHORIZATION')
             if token:
-                payload = jwt.decode(token,  settings.SECRET_KEY, algorithms='HS256')
-                if payload.get('username') != 'admin':
+                payload = jwt.decode(token, settings.SECRET_KEY, algorithms='HS256')
+                if payload.get('username') not in ['admin', ]:
                     if request.method == 'DELETE':
                         return JsonResponse({
                             "code": 300,
@@ -35,7 +33,7 @@ class IsDeleteMiddleWare(MiddlewareMixin):
                     #         "msg": "演示环境非管理员权限禁止新增，只能执行测试任务",
                     #         "data": None
                     #     }, status=200)
-                    elif request.method == 'PUT':
+                    elif request.method == 'PUT' and payload.get('username') != 'wfq123456':
                         if request.path not in ['/user/environment',
                                                 '/user/project/put',
                                                 '/ui/config/put/status']:
@@ -44,5 +42,6 @@ class IsDeleteMiddleWare(MiddlewareMixin):
                                 "msg": "演示环境非管理员权限禁止修改，只能执行测试任务",
                                 "data": None
                             }, status=200)
+
     def process_response(self, request, response):
         return response
