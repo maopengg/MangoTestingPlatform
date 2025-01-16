194720d0b385e61636b0923fae99fe1aa46a3a2e
毛鹏 <729164035@qq.com>
2025-01-16 16:13:56 +0800

    修复一个问题；

--- MangoActuator/src/services/ui/service/test_case.py
+++ MangoActuator/src/services/ui/service/test_case.py
@@ -107,7 +107,7 @@ class TestCase(PageSteps):
 
     async def case_posterior(self, posterior_sql: list[dict]):
         for sql in posterior_sql:
-            if self.mysql_connect:
+            if self.mysql_connect and sql.get('sql', None) is not None:
                 self.mysql_connect.condition_execute(sql.get('sql'))
         await self.sava_videos()
 
