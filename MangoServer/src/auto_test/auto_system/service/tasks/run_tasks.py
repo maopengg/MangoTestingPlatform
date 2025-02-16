6e3e7a32c46960c8b26bbf03ef59c143107c9f67
毛鹏 <729164035@qq.com>
2025-02-16 22:18:22 +0800

    提交，已经完成API接口的逻辑；

--- MangoServer/src/auto_test/auto_system/service/tasks/run_tasks.py
+++ MangoServer/src/auto_test/auto_system/service/tasks/run_tasks.py
@@ -46,38 +46,18 @@ class RunTasks:
 
     @classmethod
     def distribute(cls, tasks: Tasks):
-        if tasks.type == AutoTestTypeEnum.API.value:
-            tasks_details = TasksDetails.objects.filter(task=tasks.id)
+        tasks_details = TasksDetails.objects.filter(task=tasks.id)
+        for task in tasks_details:
             add_tasks = AddTasks(
                 project_product=tasks.project_product.id,
                 test_env=tasks.test_env,
                 is_notice=tasks.is_notice,
                 user_id=tasks.case_people.id,
-                _type=AutoTestTypeEnum.API.value,
                 tasks_id=tasks.id,
             )
-            add_tasks.add_test_suite_details([tasks.case_id for tasks in tasks_details])
-        elif tasks.type == AutoTestTypeEnum.UI.value:
-            tasks_details = TasksDetails.objects.filter(task=tasks.id)
-            add_tasks = AddTasks(
-                project_product=tasks.project_product.id,
-                test_env=tasks.test_env,
-                is_notice=tasks.is_notice,
-                user_id=tasks.case_people.id,
-                _type=AutoTestTypeEnum.UI.value,
-                tasks_id=tasks.id,
-            )
-            add_tasks.add_test_suite_details([tasks.case_id for tasks in tasks_details])
-        elif tasks.type == AutoTestTypeEnum.MangoPytest.value:
-            tasks_details = TasksDetails.objects.filter(task=tasks.id)
-            add_tasks = AddTasks(
-                project_product=tasks.project_product.id,
-                test_env=tasks.test_env,
-                is_notice=tasks.is_notice,
-                user_id=tasks.case_people.id,
-                _type=AutoTestTypeEnum.MangoPytest.value,
-                tasks_id=tasks.id,
-            )
-            add_tasks.add_test_suite_details([tasks.command for tasks in tasks_details])
-        else:
-            log.system.error('开始执行性能自动化任务')
+            if task.type == AutoTestTypeEnum.API.value:
+                add_tasks.add_test_suite_details(task.api_case.id, AutoTestTypeEnum.API)
+            elif task.type == AutoTestTypeEnum.UI.value:
+                add_tasks.add_test_suite_details(task.ui_case.id, AutoTestTypeEnum.UI)
+            else:
+                log.system.error('开始执行性能自动化任务')
