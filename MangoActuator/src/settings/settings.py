6c12ced995e2ac709901183edbb2921c953bc40c
毛鹏 <729164035@qq.com>
2025-02-26 16:44:38 +0800

    修改

--- MangoActuator/src/settings/settings.py
+++ MangoActuator/src/settings/settings.py
@@ -1,18 +1,20 @@
 import json
-
-from mango_ui import AppConfig, MenusModel
-
+import platform
 from src.tools import project_dir
+
 
 # **************************************** 下面不用管 **************************************** #
 IP = None
 PORT = None
 USERNAME = None
 PASSWORD = None
+if platform.system() == "Linux":
+    from mango_ui import AppConfig, MenusModel
 with open(project_dir.resource_path('src/settings/settings.json'), "r", encoding='utf-8') as f:
     STYLE = AppConfig(**json.loads(f.read()))
 with open(project_dir.resource_path('src/settings/menus.json'), "r", encoding='utf-8') as f:
     MENUS = MenusModel(**json.loads(f.read()))
+
 MEMORY_THRESHOLD = 100  # 控制内存高于多少就不可以执行用例，防止崩溃
 LOOP_MIX = 10  # 最大检查内存次数
 # **************************************** 上面不用管 **************************************** #
