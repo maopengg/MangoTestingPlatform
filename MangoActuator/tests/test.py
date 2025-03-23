07490e6b958bf17ccf526572d4c726a5256f691c
毛鹏 <729164035@qq.com>
2025-03-23 13:57:46 +0800

    测试

--- MangoActuator/tests/test.py
+++ MangoActuator/tests/test.py
@@ -10,7 +10,7 @@ from playwright.async_api import async_playwright
 
 async def main():
     async with async_playwright() as p:
-        browser = await p.chromium.connect("ws://172.27.91.172:3000/")
+        browser = await p.chromium.connect("ws://172.21.222.119:3000/")
         page_list = []
         for _ in range(100):
             context = await browser.new_context()
