7870b57211d8bd1fdb87d77275b857a5ccb83b65
毛鹏 <729164035@qq.com>
2025-04-16 21:00:58 +0800

    优化执行器

--- start_service.sh
+++ start_service.sh
@@ -1,8 +1,8 @@
-# shellcheck disable=SC2164
-docker rmi $(docker images -f "dangling=true" -q)
+docker-compose down
+docker container prune -f
+docker rmi $(docker images -f "dangling=true" -q) 2>/dev/null || true
 cd /code/MangoTestingPlatform
 git pull
-docker-compose down
 docker-compose build
 docker-compose up -d
 docker ps
