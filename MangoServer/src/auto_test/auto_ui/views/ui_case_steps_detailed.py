b1be00ccb61f14b74383d808fcd9107ca2a5af8f
毛鹏 <729164035@qq.com>
2025-04-25 15:54:38 +0800

    优化一些内容

--- MangoServer/src/auto_test/auto_ui/views/ui_case_steps_detailed.py
+++ MangoServer/src/auto_test/auto_ui/views/ui_case_steps_detailed.py
@@ -97,13 +97,14 @@ class UiCaseStepsDetailedViews(ViewSet):
             if steps_detailed.type == ElementOperationEnum.OPE.value:
                 steps_data_model.page_step_details_name = steps_detailed.ele_name.name if steps_detailed.ele_name else steps_detailed.ope_key
                 if steps_detailed.ope_value:
-                    steps_data_model.page_step_details_data = steps_detailed.ope_value
+                    steps_data_model.page_step_details_data = {i.get('f'): i.get('v') for i in steps_detailed.ope_value}
                     if 'locating' in steps_data_model.page_step_details_data:
                         steps_data_model.page_step_details_data.pop('locating')
             elif steps_detailed.type == ElementOperationEnum.ASS.value:
                 steps_data_model.page_step_details_name = steps_detailed.ele_name.name if steps_detailed.ele_name else steps_detailed.ope_key
                 if steps_detailed.ope_value:
-                    steps_data_model.page_step_details_data = steps_detailed.ope_value
+                    steps_data_model.page_step_details_data = {i.get('f'): i.get('v') for i in steps_detailed.ope_value}
+                    print(steps_data_model.page_step_details_data)
                     if 'value' in steps_data_model.page_step_details_data:
                         steps_data_model.page_step_details_data.pop('value')
             elif steps_detailed.type == ElementOperationEnum.SQL.value:
