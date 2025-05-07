0adbe7f5fafbf112eb6caae4389de7081d920ba5
毛鹏 <729164035@qq.com>
2025-05-07 22:56:46 +0800

    修复一些问题

--- MangoServer/src/settings/test.py
+++ MangoServer/src/settings/test.py
@@ -12,7 +12,7 @@ IS_SQLITE = False  # 是否选用sqlite作为数据源，默认使用mysql
 MYSQL_DB_NAME = 'test_mango_server'
 MYSQL_USER = 'root'
 MYSQL_PASSWORD = 'mP123456&'
-MYSQL_IP = 'db.internal'
+MYSQL_IP = 'db'
 MYSQL_PORT = 3306
 
 # ************************ DEBUG配置 ************************ #
@@ -30,7 +30,7 @@ REDIS = False
 IS_MINIO = True
 
 if IS_MINIO:
-    MINIO_STORAGE_ENDPOINT = 'minio.internal:9000'  # 访问IP+端口
+    MINIO_STORAGE_ENDPOINT = 'minio:9000'  # 访问IP+端口
     MINIO_STORAGE_USE_HTTPS = False  # 如果使用 HTTPS，设置为 True
     MINIO_STORAGE_MEDIA_BUCKET_NAME = 'mango-file'  # 桶名称
     MINIO_STORAGE_AUTO_CREATE_MEDIA_BUCKET = True  # 桶不存在时自动创建
