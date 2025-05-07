948d850697be8b05de19b629d07a06b548533b69
毛鹏 <729164035@qq.com>
2025-05-07 22:11:20 +0800

    修改一些问题

--- MangoServer/src/settings/__init__.py
+++ MangoServer/src/settings/__init__.py
@@ -68,7 +68,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',  # 跨域
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
