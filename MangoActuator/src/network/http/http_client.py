aedff799f89b7cd1c2af9a0f556271c479b3450f
毛鹏 <729164035@qq.com>
2025-05-13 14:51:01 +0800

    提交

--- MangoActuator/src/network/http/http_client.py
+++ MangoActuator/src/network/http/http_client.py
@@ -3,10 +3,10 @@
 # @Description: # @Time   : 2023-08-28 21:23
 # @Author : 毛鹏
 import copy
-import time
 import traceback
 from urllib.parse import urljoin
 
+import time
 from mangokit.apidrive import requests
 from mangokit.data_processor import EncryptionTool
 
@@ -39,13 +39,15 @@ class HttpClientApi(HttpBase):
         files = [
             ('failed_screenshot', (file_name, open(file_path, 'rb'), 'application/octet-stream'))
         ]
-        headers = copy.copy(cls.headers)
-        response = cls.post('/system/file', headers=headers, data=data, files=files)
-        if response.code == 200:
-            return True
-        else:
-            log.error(f'上传文件报错，请管理员检查，响应结果：{response.model_dump()}')
-            return False
+        for i in range(3):
+            headers = copy.copy(cls.headers)
+            response = cls.post('/system/file', headers=headers, data=data, files=files)
+            if response.code == 200:
+                return True
+            else:
+                log.error(f'上传文件报错，请管理员检查，响应结果：{response.model_dump()}')
+                cls.login(SetConfig.get_username(), SetConfig.get_password())
+        return False
 
     @classmethod
     def login(cls, username: str = None, password=None):
