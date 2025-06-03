f596ccdce08f949936380f20c8350a710421122f
毛鹏 <729164035@qq.com>
2025-06-03 15:51:42 +0800

    修复了一个开发环境的问题；

--- MangoServer/manage.py
+++ MangoServer/manage.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 
 import sys
+
+if __name__ == '__main__':
+    for i, arg in enumerate(sys.argv):
+        if '--env=' in arg:
+            os.environ['DJANGO_ENV'] = arg.split('=')[1]
+            del sys.argv[i]
+            break
 
 from src.tools import project_dir
 
@@ -10,13 +16,6 @@ from src.tools import project_dir
 def main():
     """Run administrative tasks."""
     project_dir.init_folder()
-    django_env = None
-    for i, arg in enumerate(sys.argv):
-        if '--env=' in arg:
-            django_env = arg.split('=')[1]
-            del sys.argv[i]
-            break
-    os.environ.setdefault('DJANGO_ENV', django_env or 'master')
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
     try:
         from django.core.management import execute_from_command_line
