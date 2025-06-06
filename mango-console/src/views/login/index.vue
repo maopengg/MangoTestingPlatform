ebd9073c34c03ff4116cf7c83eeaf54270ce7683
毛鹏 <729164035@qq.com>
2025-06-06 11:14:25 +0800

    修改登录页面；

--- mango-console/src/views/login/index.vue
+++ mango-console/src/views/login/index.vue
@@ -1,123 +1,40 @@
 <template>
   <div class="login-container">
-    <img class="bg-img" src="../../assets/bg.png" />
-    <div class="center">
-      <div class="left">
-        <img class="left-bg-img" src="../../assets/bg_left.png" />
-      </div>
-      <div class="form-wrapper">
-        <div v-if="baseData.isLogin === true">
-          <div class="title">账号登录</div>
-          <div class="item-wrapper mt-6">
-            <a-input
-              v-model="baseData.username"
-              allow-clear
-              placeholder="请输入用户名/手机号"
-              size="large"
-            >
-              <template #prefix>
-                <icon-mobile />
-              </template>
-            </a-input>
-          </div>
-          <div class="item-wrapper mt-4">
-            <a-input-password
-              v-model="baseData.password"
-              allow-clear
-              placeholder="请输入密码"
-              size="large"
-              @keyup.enter="onLogin"
-            >
-              <template #prefix>
-                <icon-lock />
-              </template>
-            </a-input-password>
-          </div>
-          <div class="flex-1"></div>
-          <div class="mt-10">
-            <a-button :loading="baseData.loading" class="login" type="primary" @click="onLogin">
-              登录
-            </a-button>
-          </div>
-        </div>
-        <div v-else>
-          <div class="title">注册用户</div>
-          <div class="item-wrapper mt-6">
-            <a-input
-              v-model="baseData.name"
-              allow-clear
-              placeholder="请输入用户昵称"
-              size="large"
-            />
-          </div>
-          <div class="item-wrapper mt-6">
-            <a-input
-              v-model="baseData.username"
-              allow-clear
-              placeholder="请输入登录用户名"
-              size="large"
-            >
-              <template #prefix>
-                <icon-mobile />
-              </template>
-            </a-input>
-          </div>
-          <div class="item-wrapper mt-4">
-            <a-input-password
-              v-model="baseData.password"
-              allow-clear
-              placeholder="请输入密码"
-              size="large"
-              @keyup.enter="onLogin"
-            >
-              <template #prefix>
-                <icon-lock />
-              </template>
-            </a-input-password>
-          </div>
-          <div class="item-wrapper mt-4">
-            <a-input-password
-              v-model="baseData.confirm_password"
-              allow-clear
-              placeholder="再次输入密码"
-              size="large"
-              @keyup.enter="onLogin"
-            >
-              <template #prefix>
-                <icon-lock />
-              </template>
-            </a-input-password>
-          </div>
-          <div class="flex-1"></div>
-          <div class="mt-10">
-            <a-button :loading="baseData.loading" class="login" type="primary" @click="onRegister">
-              注册
-            </a-button>
-          </div>
-        </div>
-        <div class="my-width flex-1 mt-4 mb-8">
-          <div class="flex justify-between">
-            <a-link :underline="false" type="primary">忘记密码？</a-link>
-            <a-link
-              v-if="baseData.isLogin === true"
-              :underline="false"
-              type="primary"
-              @click="register1"
-              >注册用户
-            </a-link>
-            <a-link
-              v-if="baseData.isLogin === false"
-              :underline="false"
-              type="primary"
-              @click="returnToLogin"
-              >返回登录
-            </a-link>
-          </div>
+    <div class="container">
+      <div class="drop">
+        <div class="content">
+          <h2> {{ baseData.isLogin ? '欢迎登录' : '欢迎注册' }} </h2>
+          <form>
+            <div class="inputBox" v-if="!baseData.isLogin">
+              <input type="text" placeholder="请输入昵称" v-model="baseData.name" />
+            </div>
+            <div class="inputBox">
+              <input type="text" placeholder="请输入账号" v-model="baseData.username" />
+            </div>
+            <div class="inputBox">
+              <input type="password" placeholder="请输入密码" v-model="baseData.password" />
+            </div>
+            <div class="inputBox" v-if="!baseData.isLogin">
+              <input
+                type="password"
+                placeholder="请输入二次确认密码"
+                v-model="baseData.confirm_password"
+              />
+            </div>
+            <div class="inputBox">
+              <a-button type="submit" @click="baseData.isLogin ? onLogin() : onRegister()">{{
+                baseData.isLogin ? '登录' : '注册'
+              }}</a-button>
+            </div>
+          </form>
         </div>
       </div>
+      <a-button @click="register1" class="btns">注册</a-button>
+      <a-button class="btns signup" @click="Message.warning('请联系管理修改密码！')"
+        >忘记密码</a-button
+      >
     </div>
     <div class="bottom"> Copyright © 芒果味 2022-至今 Version：{{ version }} </div>
-    <!--  禁止修改作者署名-->
   </div>
 </template>
 
@@ -174,7 +91,7 @@
   }
 
   function register1() {
-    baseData.isLogin = false
+    baseData.isLogin = !baseData.isLogin
   }
 
   function returnToLogin() {
@@ -188,7 +105,7 @@
       baseData.password === '' ||
       baseData.confirm_password === ''
     ) {
-      Message.error('用户昵称，用户名，密码不允许为空')
+      Message.error('用户昵称、用户名、密码不允许为空')
       return
     } else {
       if (baseData.password !== baseData.confirm_password) {
@@ -219,7 +136,6 @@
 </script>
 
 <style lang="less" scoped>
-  @leftWith: 35%;
   .login-container {
     position: relative;
     overflow: hidden;
@@ -228,15 +144,6 @@
     display: flex;
     justify-content: center;
     align-items: center;
-
-    .bg-img {
-      width: 100%;
-      height: 100%;
-      position: absolute;
-      top: 0;
-      left: 0;
-    }
-
     .bottom {
       position: fixed;
       left: 0;
@@ -244,68 +151,292 @@
       bottom: 3%;
       font-size: 14px;
       font-weight: bold;
-      color: #333;
+      color: #111;
       text-align: center;
+    }
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    font-family: 'Poppins', sans-serif;
+  }
+
+  body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    background-color: #eff0f4;
+  }
+
+  .container {
+    display: flex;
+    position: relative;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+    left: 0;
+  }
+
+  .container .drop {
+    position: relative;
+    width: 450px;
+    height: 450px;
+    box-shadow: inset 20px 20px 20px rgba(0, 0, 0, 0.05), 25px 35px 20px rgba(0, 0, 0, 0.05),
+      25px 30px 30px rgba(0, 0, 0, 0.05), inset -20px -20px 25px rgba(255, 255, 255, 0.9);
+    transition: 0.5s ease-in-out;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 52% 48% 33% 67% / 38% 45% 55% 62%;
+  }
+
+  .container .drop:hover {
+    border-radius: 50%;
+  }
+
+  .container .drop::before {
+    content: '';
+    position: absolute;
+    top: 50px;
+    left: 85px;
+    width: 35px;
+    height: 35px;
+    border-radius: 50%;
+    background-color: #fff;
+    opacity: 0.9;
+  }
+
+  .container .drop::after {
+    content: '';
+    position: absolute;
+    top: 90px;
+    left: 110px;
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    background-color: #fff;
+    opacity: 0.9;
+  }
+
+  .container .drop .content {
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+    padding: 40px;
+    gap: 15px;
+  }
+
+  .container .drop .content h2 {
+    position: relative;
+    color: #333;
+    font-size: 1.5em;
+  }
+
+  .container .drop .content form {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .container .drop .content form .inputBox {
+    position: relative;
+    width: 225px;
+    box-shadow: inset 2px 5px 10px rgba(0, 0, 0, 0.1), inset -2px -5px 10px rgba(255, 255, 255, 1),
+      15px 15px 10px rgba(0, 0, 0, 0.05), 15px 10px 15px rgba(0, 0, 0, 0.05);
+    border-radius: 25px;
+  }
+
+  .container .drop .content form .inputBox::before {
+    content: '';
+    position: absolute;
+    top: 8px;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 65%;
+    height: 5px;
+    background-color: rgba(255, 255, 255, 0.5);
+    border-radius: 5px;
+  }
+
+  .container .drop .content form .inputBox input {
+    border: none;
+    outline: none;
+    background-color: transparent;
+    width: 100%;
+    font-size: 1em;
+    padding: 10px 15px;
+  }
+
+  .container .drop .content form .inputBox input[type='submit'] {
+    color: #fff;
+    text-transform: uppercase;
+    cursor: pointer;
+    letter-spacing: 0.1em;
+    font-weight: 500;
+  }
+
+  .container .drop .content form .inputBox:last-child {
+    width: 120px;
+    background-color: #ff0f5b;
+    box-shadow: inset 2px 5px 10px rgba(0, 0, 0, 0.1), 15px 15px 10px rgba(0, 0, 0, 0.05),
+      15px 10px 15px rgba(0, 0, 0, 0.05);
+    transition: 0.5s;
+  }
+
+  .container .drop .content form .inputBox:last-child:hover {
+    width: 150px;
+  }
+
+  .btns {
+    width: 120px;
+    height: 120px;
+    right: -120px;
+    bottom: 0;
+    background-color: #c61dff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    text-decoration: none;
+    color: #fff;
+    line-height: 1.2em;
+    letter-spacing: 0.1em;
+    font-size: 0.8em;
+    transition: 0.25s;
+    text-align: center;
+    box-shadow: inset 10px 10px 10px rgba(190, 1, 254, 0.05), 15px 25px 10px rgba(190, 1, 254, 0.1),
+      15px 20px 20px rgba(190, 1, 254, 0.1), inset -10px -10px 15px rgba(255, 255, 255, 0.5);
+    border-radius: 44% 56% 65% 35% / 57% 58% 42% 43%;
+  }
+
+  .btns:hover {
+    border-radius: 50%;
+  }
+
+  .btns::before {
+    content: '';
+    position: absolute;
+    top: 15px;
+    left: 30px;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: #fff;
+    opacity: 0.45;
+  }
+
+  .btns.signup {
+    bottom: 150px;
+    right: -140px;
+    width: 80px;
+    height: 80px;
+    border-radius: 49% 51% 52% 48% / 63% 59% 41% 37%;
+    background-color: #01b4ff;
+    box-shadow: inset 10px 10px 10px rgba(1, 180, 255, 0.05), 15px 25px 10px rgba(1, 180, 255, 0.1),
+      15px 20px 20px rgba(1, 180, 255, 0.1), inset -10px -10px 15px rgba(255, 255, 255, 0.5);
+  }
+
+  .btns.signup::before {
+    left: 20%;
+    width: 15px;
+    height: 15px;
+  }
+
+  .btns:hover {
+    border-radius: 50%;
+  }
+
+  .login-container {
+    position: relative;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%);
+
+    .bg-img {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      object-fit: cover;
+      opacity: 0.2;
     }
 
     .center {
       position: relative;
       z-index: 9;
-      width: 70%;
-      height: 60%;
-      border-radius: 10px;
-      border: 1px solid #f5f5f5;
+      width: 900px;
+      height: 500px;
+      border-radius: 12px;
       display: flex;
-      align-items: center;
       background-color: #fff;
-      box-shadow: 0 0 5px #ececec;
+      box-shadow: 0 10px 30px rgba(0, 0, 0, 0.1);
+      overflow: hidden;
 
       .left {
-        position: relative;
-        width: 50%;
+        width: 45%;
         height: 100%;
-        padding: 20px;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        text-align: center;
+        position: relative;
 
         .left-bg-img {
           width: 100%;
           height: 100%;
-          position: absolute;
-          top: 0;
-          left: 0;
-        }
-
-        .proj-name {
-          font-size: 30px;
-          font-weight: bold;
-          flex: 1;
-          display: flex;
-          align-items: center;
-          justify-content: center;
+          object-fit: cover;
         }
       }
 
       .form-wrapper {
-        width: 50%;
-        padding: 2% 5%;
-        height: 100%;
+        width: 55%;
+        padding: 40px;
         display: flex;
         flex-direction: column;
+        justify-content: center;
 
         .title {
-          font-size: 25px;
-          font-weight: bold;
-          margin-bottom: 20px;
+          font-size: 24px;
+          font-weight: 600;
+          margin-bottom: 30px;
+          color: #333;
           text-align: center;
+        }
+
+        .item-wrapper {
+          margin-bottom: 20px;
+
+          :deep(.arco-input) {
+            border-radius: 6px;
+            height: 46px;
+          }
         }
 
         .login {
           width: 100%;
+          height: 46px;
+          border-radius: 6px;
+          font-size: 16px;
+          margin-top: 10px;
         }
       }
+    }
+
+    .bottom {
+      position: absolute;
+      bottom: 20px;
+      width: 100%;
+      text-align: center;
+      font-size: 14px;
+      color: #666;
     }
   }
 </style>
