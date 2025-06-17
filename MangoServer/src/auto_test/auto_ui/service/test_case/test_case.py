28fac18846b17cbbe7f2012fa8dfdf34a66bfe6a
毛鹏 <729164035@qq.com>
2025-06-17 16:37:06 +0800

    修改一个报错

--- MangoServer/src/auto_test/auto_ui/service/test_case/test_case.py
+++ MangoServer/src/auto_test/auto_ui/service/test_case/test_case.py
@@ -94,7 +94,7 @@ class TestCase:
             project_product_name=page_steps_detailed.page_step.project_product.name,
             module_name=page_steps_detailed.page_step.module.name,
             type=page_steps_detailed.page_step.project_product.ui_client_type,
-            url=page_steps_detailed.ele_name.page.url,
+            url=page_steps_detailed.page_step.page.url,
             switch_step_open_url=False,
             error_retry=None,
             element_list=[self.element_model(page_steps_detailed, )],
