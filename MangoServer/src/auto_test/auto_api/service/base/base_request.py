871ab88675595161439383076389cc746229c479
毛鹏 <729164035@qq.com>
2025-07-01 14:56:59 +0800

    修复已知问题

--- MangoServer/src/auto_test/auto_api/service/base/base_request.py
+++ MangoServer/src/auto_test/auto_api/service/base/base_request.py
@@ -79,33 +79,3 @@ class BaseRequest:
         end = time.time() - s
         return response
 
-#
-# class BaseRequest:
-#
-#     def __init__(self):
-#         self.timeout = CacheDataValue.get_cache_value(CacheDataKeyEnum.API_TIMEOUT.name)
-#
-#     async def http(self, request_data: RequestModel) -> ResponseModel:
-#         async_requests.timeout = int(self.timeout)
-#         return await async_requests.request(
-#             method=request_data.method,
-#             url=request_data.url,
-#             headers=request_data.headers,
-#             params=request_data.params,
-#             data=request_data.data,
-#             json=request_data.json_data,
-#         )
-#
-#     @classmethod
-#     async def test_http(cls, request_data: RequestModel) -> ResponseModel:
-#         response = await async_requests.request(
-#             method=request_data.method,
-#             url=request_data.url,
-#             headers=request_data.headers,
-#             params=request_data.params,
-#             data=request_data.data,
-#             json=request_data.json_data,
-#             files=request_data.file,
-#             timeout=int(30)
-#         )
-#         return response
--- MangoServer/src/tools/obtain_test_data.py
+++ MangoServer/src/tools/obtain_test_data.py
@@ -4,17 +4,12 @@
 # @Time   : 2024-11-19 11:36
 # @Author : 毛鹏
 import os
-import shutil
 import uuid
-from urllib.parse import urljoin
 
-import requests
 from mangotools.data_processor import DataProcessor
-from requests.exceptions import MissingSchema
 
 from src.auto_test.auto_system.models import FileData
-from src.exceptions import ERROR_MSG_0025, ERROR_MSG_0024, ToolsError, ERROR_MSG_0019, ERROR_MSG_0020
-from src.settings import IS_MINIO
+from src.exceptions import ERROR_MSG_0024, ToolsError, ERROR_MSG_0019
 from src.tools import project_dir
 
 
@@ -35,28 +30,19 @@ class ObtainTestData(DataProcessor):
     def get_file(cls, **kwargs) -> str:
         """传入文件名称，返回文件对象"""
         file_name = kwargs.get('data')
-        if IS_MINIO:
-            from src.settings import MINIO_STORAGE_ENDPOINT
-            minio_host = MINIO_STORAGE_ENDPOINT
-            if minio_host is None:
-                raise ToolsError(*ERROR_MSG_0025)
-            url = urljoin(minio_host, f'/mango-file/test_file/{file_name}')
-            try:
-                response = requests.get(url, proxies={'http': None, 'https': None}, )
-                if response.status_code != 200:
-                    raise ToolsError(*ERROR_MSG_0024, value=(file_name,))
-                file_path = os.path.join(project_dir.upload(), file_name)
-                with open(file_path, 'wb') as f:
-                    f.write(response.content)
-                return str(file_path)
-            except FileNotFoundError:
-                raise ToolsError(*ERROR_MSG_0020)
-            except MissingSchema:
-                raise ToolsError(*ERROR_MSG_0019, value=(file_name,))
-        else:
+        file_path = os.path.join(project_dir.upload(), file_name)
+        try:
             file_data = FileData.objects.get(name=file_name)
-            shutil.copy2(
-                str(os.path.join(project_dir.root_path(), 'mango-file', file_data.test_file.path)),
-                str(os.path.join(project_dir.upload(), file_name))
-            )
-            return str(os.path.join(project_dir.upload(), file_name))
+            if not file_data.test_file:
+                raise ToolsError(*ERROR_MSG_0019, value=(file_name,))
+
+            with file_data.test_file.open('rb') as remote_file:
+                with open(file_path, 'wb') as local_file:
+                    for chunk in remote_file.chunks(chunk_size=8192):
+                        local_file.write(chunk)
+
+            return str(file_path)
+        except FileData.DoesNotExist:
+            raise ToolsError(*ERROR_MSG_0019, value=(file_name,))
+        except IOError as e:
+            raise ToolsError(*ERROR_MSG_0024, value=(file_name,))
