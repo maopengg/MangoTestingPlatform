8abc58e3d7d3560877719d0ef1a2edcd5a878000
毛鹏 <729164035@qq.com>
2025-07-11 18:16:50 +0800

    接口兼容多产品

---
+++ MangoServer/data_cleanup_scripts/v_5_7_5_2025_07_11.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# @Project: 芒果测试平台
+# @Description: 
+# @Time   : 2025-01-24 11:36
+# @Author : 毛鹏
+
+from mangotools.models import MethodModel
+from src.auto_test.auto_api.models import ApiCaseDetailed, ApiCase
+
+
+def page_steps_detailed():
+    for i in ApiCase.objects.all():
+        ope_value_list = []
+        if isinstance(i.ope_value, dict):
+            for key, value in i.ope_value.items():
+                if key == 'locating' or key == 'actual':
+                    ope_value_list.append(MethodModel(f=key, v=value, d=False).model_dump())
+                else:
+                    ope_value_list.append(MethodModel(f=key, v=value, d=True).model_dump())
+
+            i.ope_value = ope_value_list
+            i.save()
+
+
+def main_5_5():
+    page_steps_detailed()
