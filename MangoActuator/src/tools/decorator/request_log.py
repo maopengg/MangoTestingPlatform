30d465e454f140e1b674a3bd91ffa96b07d2d1a8
毛鹏 <729164035@qq.com>
2025-07-23 12:00:39 +0800

    优化代码

--- MangoActuator/src/tools/decorator/request_log.py
+++ MangoActuator/src/tools/decorator/request_log.py
@@ -25,12 +25,12 @@ def request_log(is_serialize=True):
                 else:
                     return ResponseModel(**response.json())
             except requests.exceptions.JSONDecodeError:
-                return ResponseModel(code=300, msg='响应的数据非json格式，请检查后端服务是否可以正常运行~',
+                return ResponseModel(code=-300, msg='响应的数据非json格式，请检查后端服务是否可以正常运行~',
                                      data=response.text if response else None)
             except Exception as error:
                 log.error(f'HTTP请求返回数据异常-1，响应：{response.text if response else response}')
                 log.debug(f'HTTP请求返回数据异常-2，类型：{type(error)}，详情：{error}，明细：{traceback.format_exc()}')
-                return ResponseModel(code=300, msg='请求发送未知错误，请打开调式，再次触发这个操作，然后发送日志给管理员！',
+                return ResponseModel(code=-300, msg='请求发送未知错误，请打开调式，再次触发这个操作，然后发送日志给管理员！',
                                      data=str(error))
 
         return wrapper
