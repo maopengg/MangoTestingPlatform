c7a908474c0ce8bfda9bd6107367f9a0db5ca7b8
毛鹏 <729164035@qq.com>
2025-07-24 19:34:17 +0800

    修改一些内容

--- MangoServer/src/tools/view/model_crud.py
+++ MangoServer/src/tools/view/model_crud.py
@@ -3,7 +3,6 @@
 # @Description: 封装了分页查询，单条查询和增删改查
 # @Time   : 2023-02-08 8:30
 # @Author : 毛鹏
-import json
 from threading import Thread
 
 from django.core.exceptions import FieldError, FieldDoesNotExist
@@ -17,7 +16,7 @@ from src.exceptions import ToolsError
 from src.tools.decorator.error_response import error_response
 from src.tools.log_collector import log
 from src.tools.view import *
-from .mode import get, post, put, delete
+from .mode import get, post, put, delete, inside_post, inside_put, inside_delete
 
 
 class ModelCRUD(GenericAPIView):
@@ -34,9 +33,11 @@ class ModelCRUD(GenericAPIView):
     @error_response('system')
     def get(self, request: Request):
         from src.auto_test.auto_system.models import ProjectProduct
+        from src.auto_test.auto_pytest.models import PytestProduct
         return get(self,
                    request=request,
                    project_product=ProjectProduct,
+                   pytest_product=PytestProduct,
                    log=log,
                    field_does_note_xist=FieldDoesNotExist,
                    s3_error=S3Error,
@@ -78,23 +79,36 @@ class ModelCRUD(GenericAPIView):
                       tools_error=ToolsError,
                       )
 
+    @classmethod
+    def inside_post(cls, data: dict) -> dict:
+        return inside_post(cls,
+                           data=data,
+                           log=log,
+                           tools_error=ToolsError,
+                           m_0116=RESPONSE_MSG_0116
+                           )
+
+    @classmethod
+    def inside_put(cls, _id: int, data: dict) -> dict:
+        return inside_put(cls,
+                          _id=_id,
+                          data=data,
+                          log=log,
+                          tools_error=ToolsError,
+                          m_0116=RESPONSE_MSG_0116
+                            )
+
+    @classmethod
+    def inside_delete(cls, _id: int) -> None:
+        return inside_delete(cls, _id=_id)
+
     def asynchronous_callback(self, parent_id: int = None):
-        """
-        反射的后置处理
-        """
         if hasattr(self, 'callback'):
             th = Thread(target=self.callback, args=(parent_id,))
             th.start()
 
+    @classmethod
     def paging_list(cls, size: int, current: int, books: QuerySet, serializer) -> tuple[list, int]:
-        """
-        分页
-        @param size:
-        @param current:现在页数
-        @param books:
-        @param serializer:
-        @return:
-        """
         count = books.count()
         if count <= int(size):
             current = 1
@@ -106,32 +120,3 @@ class ModelCRUD(GenericAPIView):
             return serializer(
                 instance=Paginator(books, size).page(current),
                 many=True).data, count
-
-    @classmethod
-    def inside_post(cls, data: dict) -> dict:
-        serializer = cls.serializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return serializer.data
-        else:
-            log.system.error(f'执行内部保存时报错，请检查！数据：{data}, 报错信息：{json.dumps(serializer.errors)}')
-            raise ToolsError(*RESPONSE_MSG_0116, value=(serializer.errors,))
-
-    @classmethod
-    def inside_put(cls, _id: int, data: dict) -> dict:
-        serializer = cls.serializer(instance=cls.model.objects.get(pk=_id), data=data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return serializer.data
-        else:
-            log.system.error(f'执行内部修改时报错，请检查！id:{_id}, 数据：{data}, 报错信息：{str(serializer.errors)}')
-            raise ToolsError(*RESPONSE_MSG_0117, value=(serializer.errors,))
-
-    @classmethod
-    def inside_delete(cls, _id: int) -> None:
-        """
-        删除一条记录
-        @param _id:
-        @return:
-        """
-        cls.model.objects.get(id=_id).delete()
