4046fab9107b4cb7a2300a2afa44ff4d470f6175
毛鹏 <729164035@qq.com>
2025-07-28 10:50:29 +0800

    修复已知问题；

--- MangoActuator/src/services/api/base_tools/http_request.py
+++ MangoActuator/src/services/api/base_tools/http_request.py
@@ -5,55 +5,55 @@
 import json
 import time
 
-from aiohttp import ClientSession
-from aiohttp.client_reqrep import ClientResponse
-
-
-class HTTPRequest:
-
-    @classmethod
-    async def http_get(cls,
-                       session: ClientSession,
-                       url: str,
-                       headers: str) -> tuple[ClientResponse, float]:
-        headers = json.loads(headers)
-        s = time.time()
-        response = await session.get(url, headers=headers)
-        response_time = time.time() - s
-        return response, response_time
-
-    @classmethod
-    async def http_post(cls,
-                        session: ClientSession,
-                        url: str,
-                        headers: str,
-                        data=None) -> tuple[ClientResponse, float]:
-        headers = json.loads(headers)
-        s = time.time()
-        response = await session.post(url, data=data, headers=headers)
-        response_time = time.time() - s
-        return response, response_time
-
-    @classmethod
-    async def http_delete(cls,
-                          session: ClientSession,
-                          url: str,
-                          headers: str,
-                          params=None) -> tuple[ClientResponse, float]:
-        headers = json.loads(headers)
-        s = time.time()
-        response = await session.delete(url, params=params, headers=headers)
-        response_time = time.time() - s
-        return response, response_time
-
-    @classmethod
-    async def http_put(cls,
-                       session: ClientSession,
-                       url: str,
-                       headers: str,
-                       data=None) -> tuple[ClientResponse, float]:
-        headers = json.loads(headers)
-        s = time.time()
-        response = await session.put(url, data=data, headers=headers)
-        response_time = time.time() - s
-        return response, response_time
+# from aiohttp import ClientSession
+# from aiohttp.client_reqrep import ClientResponse
+#
+#
+# class HTTPRequest:
+#
+#     @classmethod
+#     async def http_get(cls,
+#                        session: ClientSession,
+#                        url: str,
+#                        headers: str) -> tuple[ClientResponse, float]:
+#         headers = json.loads(headers)
+#         s = time.time()
+#         response = await session.get(url, headers=headers)
+#         response_time = time.time() - s
+#         return response, response_time
+#
+#     @classmethod
+#     async def http_post(cls,
+#                         session: ClientSession,
+#                         url: str,
+#                         headers: str,
+#                         data=None) -> tuple[ClientResponse, float]:
+#         headers = json.loads(headers)
+#         s = time.time()
+#         response = await session.post(url, data=data, headers=headers)
+#         response_time = time.time() - s
+#         return response, response_time
+#
+#     @classmethod
+#     async def http_delete(cls,
+#                           session: ClientSession,
+#                           url: str,
+#                           headers: str,
+#                           params=None) -> tuple[ClientResponse, float]:
+#         headers = json.loads(headers)
+#         s = time.time()
+#         response = await session.delete(url, params=params, headers=headers)
+#         response_time = time.time() - s
+#         return response, response_time
+#
+#     @classmethod
+#     async def http_put(cls,
+#                        session: ClientSession,
+#                        url: str,
+#                        headers: str,
+#                        data=None) -> tuple[ClientResponse, float]:
+#         headers = json.loads(headers)
+#         s = time.time()
+#         response = await session.put(url, data=data, headers=headers)
+#         response_time = time.time() - s
+#         return response, response_time
