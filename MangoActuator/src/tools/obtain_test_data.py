59ff4ff96e38f9a6d418f44969b4a712e55c2c8a
毛鹏 <729164035@qq.com>
2025-08-04 16:49:43 +0800

    修改帮助文档

--- MangoActuator/src/tools/obtain_test_data.py
+++ MangoActuator/src/tools/obtain_test_data.py
@@ -28,9 +28,8 @@ class ObtainTestData(DataProcessor):
         return str(uuid.uuid4())
 
     @classmethod
-    def get_file(cls, **kwargs) -> str:
+    def get_file(cls, file_name) -> str:
         """传入文件名称，返回文件"""
-        file_name = kwargs.get('data')
         HTTP.not_auth.download_file(file_name)
         file_path = os.path.join(project_dir.upload(), file_name)
         if os.path.exists(file_path):
