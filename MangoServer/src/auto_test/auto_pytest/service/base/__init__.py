e5c5ab8f3a70720eedb20eeda9fca79e0e4a9b9b
毛鹏 <729164035@qq.com>
2025-08-31 16:46:05 +0800

    优化docker安装流程

--- MangoServer/src/auto_test/auto_pytest/service/base/__init__.py
+++ MangoServer/src/auto_test/auto_pytest/service/base/__init__.py
@@ -3,3 +3,19 @@
 # @Description: 
 # @Time   : 2025-02-18 13:04
 # @Author : 毛鹏
+from mangotools.mangos import GitRepoOperator
+from src.auto_test.auto_system.models import CacheData
+from src.enums.system_enum import CacheDataKeyEnum
+from src.exceptions import PytestError, ERROR_MSG_0043
+from src.tools import project_dir
+from src.tools.log_collector import log
+
+
+def git_obj() -> GitRepoOperator:
+    repo_url = CacheData.objects.get(key=CacheDataKeyEnum.PYTEST_GIT_URL.name).value
+    username = CacheData.objects.get(key=CacheDataKeyEnum.PYTEST_GIT_USERNAME.name).value
+    password = CacheData.objects.get(key=CacheDataKeyEnum.PYTEST_GIT_PASSWORD.name).value
+    if repo_url is None or repo_url == "" or username is None or username == "" or password is None or password == "":
+        raise PytestError(*ERROR_MSG_0043)
+    repo = GitRepoOperator(repo_url, project_dir.root_path(), log.pytest, username, password)
+    return repo
