4fa4eb126e8d4800da87d2c8230ec93cdee44985
毛鹏 <729164035@qq.com>
2025-09-01 12:47:49 +0800

    修复一个问题

--- start_service.sh
+++ start_service.sh
@@ -3,7 +3,7 @@ docker container prune -f
 docker rmi $(docker images -f "dangling=true" -q) 2>/dev/null || true
 cd /code/MangoTestingPlatform
 git pull
-docker-compose --env-file .env build
+docker-compose build
 docker-compose up -d
 docker ps
 
