e9cfd19a9d908edc942dc03b3d8db07e438b0ab9
毛鹏 <729164035@qq.com>
2025-09-02 11:27:44 +0800

    优化打包脚本

--- start_service.sh
+++ start_service.sh
@@ -1,9 +1,9 @@
 docker-compose down
 docker container prune -f
 docker rmi $(docker images -f "dangling=true" -q) 2>/dev/null || true
-cd /code/MangoTestingPlatform
 git pull
 docker-compose build
 docker-compose up -d
 docker ps
-
+docker logs mangotestingplatform-mango_server-1
+docker logs mangotestingplatform-mango_actuator-1
