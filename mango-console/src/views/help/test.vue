a9add249339709f2fa87d5d6b576b05f420c53bf
毛鹏 <729164035@qq.com>
2025-10-09 14:33:43 +0800

    修改版本

--- mango-console/src/views/help/test.vue
+++ mango-console/src/views/help/test.vue
@@ -1,1 +1,17 @@
-    
+<template>
+  <div>
+    <a-card :title="value">
+      <a-input :v-model="content" placeholder="请输入信息" />
+      <a-button @click="sendMessage">发送</a-button>
+    </a-card>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { ref } from 'vue'
+
+  const value = ref('测试页面')
+  const content = ref('')
+</script>
+
+<style scoped></style>
