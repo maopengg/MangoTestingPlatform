32c94ad2629f32997fd21d7547328eb33dc1f932
毛鹏 <729164035@qq.com>
2025-10-22 16:02:34 +0800

    增加操作日志

--- MangoServer/src/auto_test/auto_system/service/testcounter.py
+++ MangoServer/src/auto_test/auto_system/service/testcounter.py
@@ -4,11 +4,11 @@
 # @Time   : 2025-10-17 10:44
 # @Author : 毛鹏
 from src.auto_test.auto_api.models import ApiCaseDetailedParameter, ApiCaseDetailed
+from src.auto_test.auto_pytest.models import PytestCase
 from src.auto_test.auto_system.models import TestSuiteDetails
 from src.enums.tools_enum import StatusEnum
 from src.enums.tools_enum import TestCaseTypeEnum
 from src.models.system_model import CaseCounterModel
-from src.auto_test.auto_pytest.models import PytestCase
 
 
 class TestCounter:
@@ -31,6 +31,9 @@ class TestCounter:
     @staticmethod
     def res_api(test_suite_details) -> CaseCounterModel:
         case_counter = CaseCounterModel()
+        if not test_suite_details.result_data:
+            case_counter.fail += test_suite_details.case_sum
+            return case_counter
         for r in test_suite_details.result_data:
             if r.get('status') == StatusEnum.SUCCESS.value:
                 case_counter.success += 1
@@ -41,13 +44,15 @@ class TestCounter:
     @staticmethod
     def res_pytest(test_suite_details) -> CaseCounterModel:
         case_counter = CaseCounterModel()
-        if test_suite_details:
-            for r in test_suite_details.result_data:
-                case_counter.case_sum += 1
-                if r.get('status') == StatusEnum.SUCCESS.value:
-                    case_counter.success += 1
-                else:
-                    case_counter.fail += 1
+        if not test_suite_details.result_data:
+            case_counter.fail += test_suite_details.case_sum
+            return case_counter
+        for r in test_suite_details.result_data:
+            case_counter.case_sum += 1
+            if r.get('status') == StatusEnum.SUCCESS.value:
+                case_counter.success += 1
+            else:
+                case_counter.fail += 1
         return case_counter
 
     @staticmethod
