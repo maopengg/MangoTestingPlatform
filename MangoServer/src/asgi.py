4eecfebebc352262ba392769fb649dafbc9a575a
毛鹏 <729164035@qq.com>
2025-11-18 16:42:36 +0800

    Revert "优化"
    This reverts commit 0bc15c0d07e2a64183d0d1899fc44b40fcab2436.

--- MangoServer/src/asgi.py
+++ MangoServer/src/asgi.py
@@ -6,20 +6,32 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
+# 处理路由
+
 import os
-import django
-from django.core.asgi import get_asgi_application
 
-# 必须在最前面设置
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
-
-# 初始化 Django
-django.setup()
-
-# 现在导入其他模块
+import sys
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from django.core.management import call_command
+
 from . import routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
+
+# 添加项目路径到系统路径
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+
+# 检查并执行迁移
+def migrate():
+    from django import setup
+    setup()
+    call_command('migrate')
+
+
+migrate()
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
