5f3611f75fe3b7b9ed0b2044073d959ba51ebbf9
毛鹏 <729164035@qq.com>
2025-11-20 15:51:47 +0800

    修复一个问题

--- MangoServer/src/auto_test/auto_api/service/base/api_base_test_setup/__init__.py
+++ MangoServer/src/auto_test/auto_api/service/base/api_base_test_setup/__init__.py
@@ -82,9 +82,8 @@ class APIBaseTestSetup(PublicBase):
                                 file_name = self.test_data.identify_parentheses(v)[0].replace('(', '').replace(')', '')
                                 mime_type, _ = mimetypes.guess_type(file_path)
                                 if mime_type is None:
-                                    file.append((k, (file_name, open(file_path, 'rb'),)))
-                                else:
-                                    file.append((k, (file_name, open(file_path, 'rb'), mime_type)))
+                                    mime_type = 'application/octet-stream'
+                                file.append((k, (file_name, open(file_path, 'rb'), mime_type)))
                         request_data_model.file = file
                 else:
                     value = self.test_data.replace(value)
