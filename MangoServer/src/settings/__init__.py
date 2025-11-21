19034782e8e0c8d91cf34649c180c4f1ad651a32
毛鹏 <729164035@qq.com>
2025-11-21 10:53:50 +0800

    修改版本

--- MangoServer/src/settings/__init__.py
+++ MangoServer/src/settings/__init__.py
@@ -8,7 +8,7 @@ from pathlib import Path
 
 from ..enums.tools_enum import SystemEnvEnum
 
-VERSION = '5.8.6'
+VERSION = '5.8.7'
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # **********************************************************************************************************************
 DJANGO_ENV = os.getenv('DJANGO_ENV', 'master')
