6c606dc0d93c9e2d99fd36ef1d0991f049cd18c6
毛鹏 <729164035@qq.com>
2025-11-28 16:34:26 +0800

    修改docker为daphne启动的尝试

--- MangoServer/src/asgi.py
+++ MangoServer/src/asgi.py
@@ -6,32 +6,18 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
-# 处理路由
 
 import os
-
-import sys
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
+import django
 from django.core.asgi import get_asgi_application
-from django.core.management import call_command
-
-from . import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
 
-# 添加项目路径到系统路径
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+django.setup()
 
-
-# 检查并执行迁移
-def migrate():
-    from django import setup
-    setup()
-    call_command('migrate')
-
-
-migrate()
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from . import routing
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
